bcc0413c51079d50ff7262213afa9865667325cc
patrik <patlil@kth.se>
2015-12-24 12:18:17 +0100

    day 3 haskell

---
+++ day3_p/day3.hs
@@ -0,0 +1,22 @@
+import Kattio
+import Data.Set
+
+type Point = (Int, Int)
+
+direction '^' = (0, 1)
+direction '<' = (-1, 0)
+direction '>' = (1, 0)
+direction 'v' = (0, -1)
+
+solve :: String -> Point -> Set Point -> Int
+solve [] _ visited = size visited
+solve (z:zs) (x,y) visited = let dx = fst $ direction z
+                                 dy = snd $ direction z
+                                 nextPoint = (x + dx, y + dy)
+                             in solve zs nextPoint (insert nextPoint visited)
+
+main = do lineOp <- getLineOp
+          case lineOp of
+             [] -> return ()
+             [line] -> do print $ solve line (0, 0) (singleton (0, 0))
+                          main
