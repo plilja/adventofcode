64e3b0d3813d699af884b54c1acc372c04b409b8
patrik <patlil@kth.se>
2016-06-19 11:53:15 +0200

    day 6 step 2

--- day6_p/day6.py
+++ day6_p/day6.py
@@ -1,36 +1,44 @@
 from collections import defaultdict
 import sys
 
-grid = defaultdict(lambda : defaultdict(int))
+def solve(lines, toggle, turn_on, turn_off):
+    grid = defaultdict(lambda : defaultdict(int))
 
-def transform(y1, x1, y2, x2, f):
-    for y in range(y1, y2 + 1):
-        for x in range(x1, x2 + 1):
-            grid[y][x] = f(grid[y][x])
+    def transform(y1, x1, y2, x2, f):
+        for y in range(y1, y2 + 1):
+            for x in range(x1, x2 + 1):
+                grid[y][x] = f(grid[y][x])
 
+    def coord(s):
+        [y, x] = s.split(',')
+        return (int(y), int(x))
 
-def coord(s):
-    [y, x] = s.split(',')
-    return (int(y), int(x))
-
-
-for inp in sys.stdin.readlines():
-    args = inp.split()
-    if args[0] == 'toggle':
-        [y1, x1] = coord(args[1])
-        [y2, x2] = coord(args[3])
-        transform(y1, x1, y2, x2, lambda x : (x + 1) % 2)
-    else:
-        [y1, x1] = coord(args[2])
-        [y2, x2] = coord(args[4])
-        if args[1] == 'on':
-            transform(y1, x1, y2, x2, lambda x : 1)
+    for inp in lines:
+        args = inp.split()
+        if args[0] == 'toggle':
+            [y1, x1] = coord(args[1])
+            [y2, x2] = coord(args[3])
+            transform(y1, x1, y2, x2, toggle)
         else:
-            transform(y1, x1, y2, x2, lambda x : 0)
+            [y1, x1] = coord(args[2])
+            [y2, x2] = coord(args[4])
+            if args[1] == 'on':
+                transform(y1, x1, y2, x2, turn_on)
+            else:
+                transform(y1, x1, y2, x2, turn_off)
 
-ans = 0
-for y in range(0, 1000):
-    for x in range(0, 1000):
-        ans += grid[y][x]
+    ans = 0
+    for y in range(0, 1000):
+        for x in range(0, 1000):
+            ans += grid[y][x]
+    return ans
 
-print(ans)
+
+step = int(sys.argv[1])
+lines = sys.stdin.readlines()
+if step == 1:
+    print(solve(lines, lambda x : (x + 1) % 2, lambda x : 1, lambda x : 0))
+elif step == 2:
+    print(solve(lines, lambda x : x + 2, lambda x : x + 1, lambda x : max(0, x - 1)))
+else:
+    raise ValueError()
