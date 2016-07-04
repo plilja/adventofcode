62f9a623eb7b236d161eca5ecf5d7c7197b76537
patrik <patlil@kth.se>
2016-07-04 22:51:18 +0200

    day 15

---
+++ day15_p/day15.py
@@ -0,0 +1,39 @@
+import sys
+
+def solve():
+    def read_input():
+        ings = []
+        for s in sys.stdin.readlines():
+            s = s.replace(',', '')
+            [ing, _, capacity, _, durability, _, flavor, _, texture, _, calories] = s.split()
+            ings += [list(map(int, [capacity, durability, flavor, texture, calories]))]
+        return ings
+
+    def f(ings, rem, recipe = []):
+        if ings == []:
+            res = [0, 0, 0, 0, 0]
+            for r in recipe:
+                for i in range(0, len(res)):
+                    res[i] += r[i]
+            for i in res:
+                if i < 0:
+                    return 0
+            a = 1
+            for i in res[:-1]:
+                a *= i
+            return a
+
+        best = 0
+        if len(ings) == 1:
+            poss = [rem]
+        else:
+            poss = range(0, rem + 1)
+
+        for i in poss:
+            tmp = list(map(lambda x : i * x, ings[0]))
+            best = max(best, f(ings[1:], rem - i, recipe + [tmp]))
+        return best
+
+    return f(read_input(), 100)
+
+print(solve())
