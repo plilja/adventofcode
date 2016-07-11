63b5dc66c25bf4e6847d0317c264d71b3ee6e252
patrik <patlil@kth.se>
2016-07-11 22:02:51 +0200

    day 1 step 2

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,8 +4,11 @@ toInt '(' = 1
 toInt ')' = -1
 toInt _ = 0
 
-solve = foldl (+) 0 . map toInt
+solve1 = foldl (+) 0 . map toInt
+
+solve2 xs = length $ takeWhile (>= 0) $ scanl (+) 0 $ map toInt xs
 
 main = do lines <- liftM lines getContents
-          mapM_ print (map solve lines)
+          mapM_ print (map solve1 lines)
+          mapM_ print (map solve2 lines)
 
