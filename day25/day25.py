54e1150e981418d3f86af134a938ecabee26bf5f
patrik <patlil@kth.se>
2016-07-11 23:14:41 +0200

    day 25 step 1

---
+++ day25/day25.py
@@ -0,0 +1,13 @@
+def solve(row, col):
+    i = 1
+    prev = 20151125
+    while True:
+        for j in range(0, i + 1):
+            t = 252533 * prev % 33554393
+            if i - j == col - 1 and j == row - 1:
+                return t
+            prev = t
+        i += 1
+
+[row, col] = list(map(int, input().split()))
+print(solve(row, col))
