93c79c7850000c63ca15a8191707f364a719a59d
patrik <patlil@kth.se>
2016-07-14 21:09:58 +0200

    day 2 step 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,15 +1,24 @@
 import Control.Monad
 import Data.List.Split
+import Data.List
 
 calcArea [l, w, h] = 2*l*w + 2*w*h + 2*h*l
 
 calcSlack [l, w, h] = minimum [l*w, w*h, h*l]
 
-solve :: String -> Int
-solve xs = let splitted = splitOn "x" xs
-               asInts = map (\x -> read x :: Int) splitted
+dim :: String -> [Int]
+dim xs = map read $ splitOn "x" xs
+
+step1 :: String -> Int
+step1 xs = let asInts = dim xs
            in calcArea asInts + calcSlack asInts
 
+step2 :: String -> Int
+step2 xs = let asInts = sort $ dim xs
+            in 2 * (asInts !! 0) + 2 * (asInts !! 1) + (product asInts)
+
 main = do lines <- liftM lines getContents
-          let res = foldl (+) 0 (map solve lines)
-          print res
+          let res1 = foldl (+) 0 (map step1 lines)
+          print res1
+          let res2 = foldl (+) 0 (map step2 lines)
+          print res2
