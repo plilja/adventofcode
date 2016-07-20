ef79a1c4cf1af8d1ec87b12c2cd9b67825a2f4e3
patrik <patlil@kth.se>
2016-07-20 22:36:39 +0200

    day 15 step 2

--- day15/day15.py
+++ day15/day15.py
@@ -1,14 +1,15 @@
 import sys
 
-def solve():
-    def read_input():
-        ings = []
-        for s in sys.stdin.readlines():
-            s = s.replace(',', '')
-            [ing, _, capacity, _, durability, _, flavor, _, texture, _, calories] = s.split()
-            ings += [list(map(int, [capacity, durability, flavor, texture, calories]))]
-        return ings
+def read_input():
+    ings = []
+    for s in sys.stdin.readlines():
+        s = s.replace(',', '')
+        [ing, _, capacity, _, durability, _, flavor, _, texture, _, calories] = s.split()
+        ings += [list(map(int, [capacity, durability, flavor, texture, calories]))]
+    return ings
 
+
+def solve(ings, pred):
     def f(ings, rem, recipe = []):
         if ings == []:
             res = [0, 0, 0, 0, 0]
@@ -21,19 +22,23 @@ def solve():
             a = 1
             for i in res[:-1]:
                 a *= i
-            return a
+            if pred(res[-1]):
+                return a
+            else:
+                return -1
 
         best = 0
         if len(ings) == 1:
             poss = [rem]
         else:
             poss = range(0, rem + 1)
-
         for i in poss:
             tmp = list(map(lambda x : i * x, ings[0]))
             best = max(best, f(ings[1:], rem - i, recipe + [tmp]))
         return best
 
-    return f(read_input(), 100)
+    return f(ings, 100, )
 
-print(solve())
+ings = read_input()
+print(solve(ings, lambda calories : True))
+print(solve(ings, lambda calories : calories == 500))
