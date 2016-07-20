2bb91c3d0b084db4d1e31bccc3eb5e37a99a520a
patrik <patlil@kth.se>
2016-07-20 22:25:45 +0200

    day 14 step 2

--- day14/day14.py
+++ day14/day14.py
@@ -1,19 +1,45 @@
 import sys
 from math import *
 
-winner_dist = 0
-
 TIME = 2503
 
+reindeers = []
 for i in sys.stdin.readlines():
     [reindeer, can, fly, speed, unit, _for1, num1, seconds1, 
             but, then, must, rest, _for2, num2, seconds2] = i.split()
-    [speed, num1, num2] = list(map(int, [speed, num1, num2]))
-    cycles = floor(TIME / (num1 + num2))
-    rem = min(num1, TIME - cycles * (num1 + num2))
-    time_travelling = cycles * num1 + rem
-    dist = speed * time_travelling
-    if dist > winner_dist:
-        winner_dist = dist
+    reindeers += [(int(speed), int(num1), int(num2))]
 
-print(winner_dist)
+
+def solve():
+    timers = [reindeers[i][1] for i in range(0, len(reindeers))]
+    dists = [0 for i in reindeers]
+    points = [0 for i in reindeers]
+    speeds = [reindeers[i][0] for i in range(0, len(reindeers))]
+    for i in range(0, TIME):
+        leaders = [0]
+        for j in range(0, len(reindeers)):
+            active_speed, num1, num2 = reindeers[j]
+            timers[j] -= 1
+            dists[j] += speeds[j]
+            if timers[j] == 0:
+                if speeds[j] == 0:
+                    speeds[j] = active_speed
+                    timers[j] = num1
+                else:
+                    speeds[j] = 0
+                    timers[j] = num2
+            if dists[j] > dists[leaders[0]]:
+                leaders = [j]
+            elif dists[j] == dists[leaders[0]] and j != leaders[0]:
+                leaders += [j]
+
+        for leader in leaders:
+            points[leader] += 1
+
+    winner_by_dist = max(dists)
+    winner_by_points = max(points)
+    return (winner_by_dist, winner_by_points)
+
+step1, step2 = solve()
+print(step1)
+print(step2)
