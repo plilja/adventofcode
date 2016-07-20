8bc3915fd9afdc49f0c06a32d52aa7e91f53d883
patrik <patlil@kth.se>
2016-07-20 20:47:19 +0200

    day 8 step 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,14 +1,28 @@
 import sys
 import re
 
-escs = [re.compile('\\\\\\\\'), re.compile('\\\\x[0-9a-f][0-9a-f]'), re.compile('\\\\"')]
+def step1(lines):
+    escs = [re.compile('\\\\\\\\'), re.compile('\\\\x[0-9a-f][0-9a-f]'), re.compile('\\\\"')]
+    ans = 0
+    for s in map(str.strip, lines):
+        ans += len(s)
+        escaped = s[1:-1]
+        for esc in escs:
+            escaped = esc.sub('_', escaped)
+        ans -= len(escaped)
+    return ans
 
-ans = 0
-for s in map(str.strip, sys.stdin.readlines()):
-    ans += len(s)
-    escaped = s[1:-1]
-    for esc in escs:
-        escaped = esc.sub('_', escaped)
-    ans -= len(escaped)
+def step2(lines):
+    escs = ['"', "\\"]
+    ans = 0
+    for s in map(str.strip, lines):
+        ans += len(s)
+        for esc in escs:
+            s = s.replace(esc, '_' + esc)
+        ans -= len(s) + 2
+    return -ans
 
-print(ans)
+
+lines = sys.stdin.readlines()
+print(step1(lines))
+print(step2(lines))
