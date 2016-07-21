b23267881e0f10ce20c333e49b6a96f0226c3130
patrik <patlil@kth.se>
2016-07-21 21:27:05 +0200

    day 16 step 2

--- day16/day16.py
+++ day16/day16.py
@@ -1,4 +1,6 @@
 import sys
+from collections import defaultdict
+from copy import copy
 
 clues = {'children': 3,
         'cats': 7,
@@ -11,26 +13,36 @@ clues = {'children': 3,
         'cars': 2,
         'perfumes': 1}
 
-def solve():
-    def read_input():
-        sues = {}
-        for s in sys.stdin.readlines():
-            args = s.replace(':', '').replace(',', '').split()
-            nr = int(args[1])
-            sues[nr] = {}
-            for i in range(2, len(args), 2):
-                sues[nr][args[i]] = int(args[i + 1])
-        return sues
 
-    def f(sues):
-        for k in sues.keys():
-            match = True
-            for (a, v) in sues[k].items():
-                if clues[a] != v:
-                    match = False
-            if match:
-                return k
+def solve(sues, predicates):
+    for k in sues.keys():
+        match = True
+        for (a, v) in sues[k].items():
+            if not predicates[a](clues[a], v):
+                match = False
+        if match:
+            return k
 
-    return f(read_input())
 
-print(solve())
+def read_input():
+    sues = {}
+    for s in sys.stdin.readlines():
+        args = s.replace(':', '').replace(',', '').split()
+        nr = int(args[1])
+        sues[nr] = {}
+        for i in range(2, len(args), 2):
+            sues[nr][args[i]] = int(args[i + 1])
+    return sues
+
+
+inp = read_input()
+
+step1 = defaultdict(lambda: lambda x, y: x == y)
+print(solve(inp, step1))
+
+step2 = copy(step1) 
+step2['cats'] = lambda x, y : x < y
+step2['trees'] = lambda x, y : x < y
+step2['pomeranians'] = lambda x, y : x > y
+step2['goldfish'] = lambda x, y : x > y
+print(solve(inp, step2))
