aecfde1dec7a5d93298620fcf8946963b4dfde6d
patrik <patlil@kth.se>
2016-07-21 22:07:35 +0200

    day 17 step 2

--- day17/day17.py
+++ day17/day17.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 
 TARGET = 150
 
-def solve():
-    containers = [int(s) for s in sys.stdin.readlines()]
+
+def step1(containers):
     m = defaultdict(lambda: defaultdict(int))
     m[-1][0] = 1
     for i in range(0, len(containers)):
@@ -15,4 +15,24 @@ def solve():
                 m[i][j] += m[i - 1][j - c]
     return m[len(containers) - 1][TARGET]
 
-print(solve())
+
+def step2(containers):
+    m = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    for k in range(0, len(containers) + 1):
+        for j in range(0, len(containers)):
+            m[k][j - 1][0] = 1
+    for k in range(1, len(containers) + 1):
+        for i in range(0, len(containers)):
+            c = containers[i]
+            for j in range(0, TARGET + 1):
+                m[k][i][j] = m[k][i - 1][j]
+                if j >= c:
+                    m[k][i][j] += m[k - 1][i - 1][j - c]
+        if m[k][len(containers) - 1][TARGET] > 0:
+            return m[k][len(containers) - 1][TARGET]
+    return 0
+
+
+containers = [int(s) for s in sys.stdin.readlines()]
+print(step1(containers))
+print(step2(containers))
