f9c180fcc70c1680d338fab6bc95e5db4fd09aee
patrik <patlil@kth.se>
2016-07-23 11:06:17 +0200

    day 20 step 2

--- day20/day20.py
+++ day20/day20.py
@@ -1,21 +1,28 @@
 from math import *
 from array import *
 
-def solve(n, lim = 10000):
+
+INF = 10**10
+
+
+def solve(n, m, fact, lim = 10000):
     arr = array('i', [10 for i in range(0, lim + 1)])
 
     for i in range(2, lim):
-        q = floor(lim / i)
+        q = min(m, floor(lim / i))
         for j in range(1, q + 1):
-            arr[j * i] += 10 * i
+            arr[j * i] += fact * i
 
     for i in range(1, lim):
         if arr[i] >= n:
             return i
 
     # didn't find solution, try increasing limit
-    return solve(n, lim * 2)
+    return solve(n, m, fact, 2 * lim)
+
 
 n = int(input())
-a = solve(n)
+a = solve(n, INF, 10)
 print(a)
+b = solve(n, 50, 11)
+print(b)
