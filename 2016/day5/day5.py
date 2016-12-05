95d5b7359a4f4826f560f697c103165645809105
plilja <patlil@kth.se>
2016-12-05 18:47:28 +0100

    Day 5 step 2

--- 2016/day5/day5.py
+++ 2016/day5/day5.py
@@ -1,18 +1,27 @@
 import hashlib
-import sys
+import itertools
 
 def step1(door_id):
-    j = 0
-    ans = ''
-    for i in range(0, 8):
-        while True:
-            digest = md5(door_id + str(j))
-            j += 1
-            if digest[:5] == '00000':
-                ans += digest[5]
-                break
-    return ans
+    hashes = itertools.islice(interesting_hashes(door_id), 8)
+    return ''.join(map(lambda x : x[5], hashes))
 
+def step2(door_id):
+    res = ['_'] * 8 
+    for h in interesting_hashes(door_id):
+        if h[5] >= '0' and h[5] <= '7':
+            i = ord(h[5]) - ord('0')
+            if res[i] == '_':
+                res[i] = h[6]
+                if not '_' in res:
+                    return ''.join(res)
+
+def interesting_hashes(door_id):
+    j = 0
+    while True:
+        digest = md5(door_id + str(j))
+        j += 1
+        if digest[:5] == '00000':
+            yield digest
 
 def md5(v):
     m = hashlib.md5()
@@ -21,3 +30,4 @@ def md5(v):
 
 door_id = input().strip()
 print(step1(door_id))
+print(step2(door_id))
