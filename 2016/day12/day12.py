d894b56f82ac3d31c751fda6156e3a6db65014de
plilja <patlil@kth.se>
2016-12-12 17:38:31 +0100

    Day 12 step 2

--- 2016/day12/day12.py
+++ 2016/day12/day12.py
@@ -1,12 +1,21 @@
 import sys
-from collections import *
-from functools import partial
 
-Machine = namedtuple('Machine', 'a b c d pc')
-
+class Machine():
+    def __init__(self, a, b, c, d, pc):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.pc = pc
+        
 def step1(inp):
     instructions = parse_instructions(inp)
     m = execute(Machine(0, 0, 0, 0, 0), instructions)
+    return m.a
+
+def step2(inp):
+    instructions = parse_instructions(inp)
+    m = execute(Machine(0, 0, 1, 0, 0), instructions)
     return m.a
 
 def parse_instructions(inp):
@@ -15,20 +24,20 @@ def parse_instructions(inp):
     else:
         xs = inp[0].strip().split()
         if xs[0] == 'cpy':
-            return [lambda m : increment_pc(set_register(m, xs[2], register_or_constant(m, xs[1])))] + parse_instructions(inp[1:])
+            return [cpy(xs[1], xs[2])] + parse_instructions(inp[1:])
         elif xs[0] == 'inc':
-            return [lambda m : increment_pc(set_register(m, xs[1], get_register(m, xs[1]) + 1))] + parse_instructions(inp[1:])
+            return [inc(xs[1])] + parse_instructions(inp[1:])
         elif xs[0] == 'dec':
-            return [lambda m : increment_pc(set_register(m, xs[1], get_register(m, xs[1]) - 1))] + parse_instructions(inp[1:])
+            return [dec(xs[1])] + parse_instructions(inp[1:])
         elif xs[0] == 'jnz':
-            return [lambda m : jump(m, register_or_constant(m, xs[1]), int(xs[2]))] + parse_instructions(inp[1:])
+            return [jnz(xs[1], xs[2])] + parse_instructions(inp[1:])
         else:
             raise ValueError('Unexpected instruction [%s]' % s)
 
 def execute(machine, instructions):
     while machine.pc < len(instructions):
         instruction = instructions[machine.pc]
-        machine = instruction(machine)
+        instruction(machine)
     return machine
 
 def register_or_constant(machine, unknown):
@@ -37,8 +46,35 @@ def register_or_constant(machine, unknown):
     else:
         return int(unknown)
 
-def increment_pc(machine):
-    return Machine(machine.a, machine.b, machine.c, machine.d, machine.pc + 1)
+def cpy(value, register):
+    def f(m):
+        v = register_or_constant(m, value)
+        set_register(m, register, v)
+        m.pc += 1
+    return lambda m: f(m)
+
+def inc(register):
+    def f(m):
+        v = get_register(m, register)
+        set_register(m, register, v + 1)
+        m.pc += 1
+    return lambda m: f(m)
+
+def dec(register):
+    def f(m):
+        v = get_register(m, register)
+        set_register(m, register, v - 1)
+        m.pc += 1
+    return lambda m: f(m)
+
+def jnz(value, delta):
+    def f(m):
+        v = register_or_constant(m, value)
+        if v == 0:
+            m.pc += 1
+        else:
+            m.pc += int(delta)
+    return lambda m: f(m)
 
 def get_register(machine, register):
     if register == 'a':
@@ -53,20 +89,19 @@ def get_register(machine, register):
 
 def set_register(machine, register, value):
     if register == 'a':
-        return Machine(value, machine.b, machine.c, machine.d, machine.pc)
-    if register == 'b':
-        return Machine(machine.a, value, machine.c, machine.d, machine.pc)
-    if register == 'c':
-        return Machine(machine.a, machine.b, value, machine.d, machine.pc)
-    if register == 'd':
-        return Machine(machine.a, machine.b, machine.c, value, machine.pc)
-    raise ValueError('Unexpected register %s' % register)
-
-def jump(machine, check, delta):
-    if check == 0:
-        return Machine(machine.a, machine.b, machine.c, machine.d, machine.pc + 1)
+        machine.a = value
+    elif register == 'b':
+        machine.b = value
+    elif register == 'c':
+        machine.c = value
+    elif register == 'd':
+        machine.d = value
     else:
-        return Machine(machine.a, machine.b, machine.c, machine.d, machine.pc + delta)
+        raise ValueError('Unexpected register %s' % register)
 
 inp = sys.stdin.readlines()
+print('Step 1')
 print(step1(inp))
+print('This one is slow. Wait for it...', file=sys.stderr)
+print('Step 2')
+print(step2(inp))
