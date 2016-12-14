26e9da63659b97b6929973dae6dd6cddf199b4c8
plilja <patlil@kth.se>
2016-12-14 16:54:50 +0100

    Day 13 step 2

--- 2016/day13/day13.py
+++ 2016/day13/day13.py
@@ -9,13 +9,38 @@ def step1(n, target_x, target_y):
         if x == target_x and y == target_y:
             return dist
 
-        neighbours = [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]
-        for x2, y2 in neighbours:
-            if is_open(n, x2, y2) and (x2, y2) not in visited:
+        for x2, y2 in neighbours(n, x, y):
+            if (x2, y2) not in visited:
                 visited |= {(x2, y2)}
                 queue += [(x2, y2, dist + 1)]
 
     return float('inf')
+
+
+def step2(n):
+    queue = [(1, 1, 0)]
+    visited = {(1, 1)}
+    ans = 0
+    while queue:
+        x, y, dist = queue[0]
+        queue = queue[1:]
+
+        if dist > 50:
+            return ans
+        ans += 1
+
+        for x2, y2 in neighbours(n, x, y):
+            if (x2, y2) not in visited:
+                visited |= {(x2, y2)}
+                queue += [(x2, y2, dist + 1)]
+
+    return ans
+
+
+def neighbours(n, x, y):
+    for x, y in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]:
+        if x >= 0 and y >= 0 and is_open(n, x, y):
+            yield (x, y)
 
 
 def is_open(n, x, y):
@@ -30,3 +55,4 @@ def is_open(n, x, y):
 n = int(input())
 [x, y] = list(map(int, input().split()))
 print(step1(n, x, y))
+print(step2(n))
