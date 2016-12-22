2192b4c4107dde929bb2f95ede9644f949e022e0
plilja <patlil@kth.se>
2016-12-22 17:43:54 +0100

    Day 21 step 2

--- 2016/day21/day21.py
+++ 2016/day21/day21.py
@@ -1,7 +1,45 @@
 import sys
 
+def step1(ops):
+    return scramble('abcdefgh', ops)
 
-def step1(start, ops):
+
+def step2(ops):
+    return unscramble('fbgdceah', ops)
+
+
+def unscramble(start, ops):
+    for i in range(0, len(ops)):
+        if 'left' in ops[i]:
+            ops[i] = ops[i].replace('left', 'right')
+        else:
+            ops[i] = ops[i].replace('right', 'left')
+        if 'move' in ops[i]:
+            parts = ops[i].split()
+            parts[2], parts[5] = parts[5], parts[2]
+            ops[i] = ' '.join(parts)
+        if 'rotate based on' in ops[i]:
+            parts = ops[i].split()
+            ops[i] = 'rotate special %s' % parts[6]
+    return scramble(start, reversed(ops))
+
+
+def scramble(start, ops):
+    def rotate_left(passwd, n):
+        n = n % len(passwd)
+        return passwd[n:] + passwd[:n]
+
+    def rotate_right(passwd, n):
+        n = n % len(passwd)
+        return passwd[-n:] + passwd[:-n]
+
+    def rotate_based_on(passwd, c):
+        x = passwd.index(c)
+        if x >= 4:
+            x += 1
+        x += 1
+        return rotate_right(passwd, x)
+
     passwd = [x for x in start]
     for op in ops:
         parts = op.split()
@@ -22,22 +60,21 @@ def step1(start, ops):
                     passwd[i] = y
                 elif passwd[i] == y:
                     passwd[i] = x
+        elif parts[0] == 'rotate' and parts[1] == 'special':
+            c = parts[2][0]
+            new_passwd = passwd
+            while rotate_based_on(new_passwd, c) != passwd:
+                new_passwd = rotate_left(new_passwd, 1)
+            passwd = new_passwd
         elif parts[0] == 'rotate':
             if parts[1] == 'based':
-                x = passwd.index(parts[6][0])
-                i = x
-                if x >= 4:
-                    x += 1
-                x += 1
-                direction = 'right'
+                passwd = rotate_based_on(passwd, parts[6][0])
             else:
                 x = int(parts[2])
-                direction = parts[1]
-            x = x % len(passwd)
-            if direction == 'left':
-                passwd = passwd[x:] + passwd[:x]
-            else:
-                passwd = passwd[-x:] + passwd[:-x]
+                if parts[1] == 'left':
+                    passwd = rotate_left(passwd, x) 
+                else:
+                    passwd = rotate_right(passwd, x) 
         else:
             assert parts[0] == 'move'
             x = int(parts[2])
@@ -49,7 +86,6 @@ def step1(start, ops):
     return ''.join(passwd)
 
 
-
-start = input()
 ops = sys.stdin.readlines()
-print(step1(start, ops))
+print(step1(ops))
+print(step2(ops))
