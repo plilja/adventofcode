c0ebff89463063742774b8f6b30dc5e5efdb09e4
Patrik Lilja <lilja.patrik@gmail.com>
2017-12-03 12:07:52 +0100

    Day 3 step 2

--- 2017/day03/day03.py
+++ 2017/day03/day03.py
@@ -1,45 +1,73 @@
-def ulam_spiral_corners():
-    corners_at_level = (1, 1, 1, 1)
-    level = 1
-    while True:
-        yield corners_at_level
-        (tr, tl, bl, br) = corners_at_level
-        corners_at_level = (br + level + 1, br + 2 * (level + 1), br + 3 * (level + 1), br + 4 * (level + 1))
-        level += 2
+from collections import defaultdict
 
 
 def step1(d):
     if d == 1:
         return 0
-    u = ulam_spiral_corners()
-    next(u) # Skip center
+    edge = 2
+    corner1 = 2
     layers = 1
-    prev = 2
-    for tr, tl, bl, br in u:
-        stop = False
-        if d <= tr:
-            corner1 = prev
-            corner2 = tr
-            stop = True
-        elif d <= tl:
-            corner1 = tr
-            corner2 = tl
-            stop = True
-        elif d <= bl:
-            corner1 = tl
-            corner2 = bl
-            stop = True
-        elif d <= br:
-            corner1 = bl
-            corner2 = br
-            stop = True
-
-        if stop:
-            center = corner1 + (corner2 - corner1) // 2
-            dist_to_center = abs(d - center)
-            return layers + dist_to_center
+    while True:
+        for i in range(0, 4):
+            corner2 = corner1 + edge
+            if i == 0:
+                corner2 -= 1
+            if d <= corner2:
+                center = corner1 + (corner2 - corner1) // 2
+                dist_to_center = abs(d - center)
+                return layers + dist_to_center
+            corner1 = corner2
+        corner1 += 1
         layers += 1
-        prev = br + 1
+        edge += 2
+
+
+def step2(d):
+    if d == 1:
+        return 2
+
+    def sum_neighbours(grid, x, y):
+        return grid[y + 1][x - 1] + \
+               grid[y + 1][x] + \
+               grid[y + 1][x + 1] + \
+               grid[y][x + 1] + \
+               grid[y][x - 1] + \
+               grid[y - 1][x - 1] + \
+               grid[y - 1][x] + \
+               grid[y - 1][x + 1]
+
+
+    grid = defaultdict(lambda: defaultdict(lambda: 0))
+    grid[0][0] = 1
+    edge = 2
+    x = 1
+    y = 1
+    while True:
+        for i in range(0, edge):
+            y -= 1
+            grid[y][x] = sum_neighbours(grid, x, y)
+            if grid[y][x] > d:
+                return grid[y][x]
+        for i in range(0, edge):
+            x -= 1
+            grid[y][x] = sum_neighbours(grid, x, y)
+            if grid[y][x] > d:
+                return grid[y][x]
+        for i in range(0, edge):
+            y += 1
+            grid[y][x] = sum_neighbours(grid, x, y)
+            if grid[y][x] > d:
+                return grid[y][x]
+        for i in range(0, edge):
+            x += 1
+            grid[y][x] = sum_neighbours(grid, x, y)
+            if grid[y][x] > d:
+                return grid[y][x]
+        x += 1
+        y += 1
+        edge += 2
+
 
 d = int(input())
 print(step1(d))
+print(step2(d))
