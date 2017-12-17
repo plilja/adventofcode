2225c3b992ed60bbba66924cc4aa94d530b586eb
Patrik Lilja <lilja.patrik@gmail.com>
2017-12-17 09:51:52 +0100

    Day 17 step 2

--- 2017/day17/day17.py
+++ 2017/day17/day17.py
@@ -1,4 +1,3 @@
-
 def step1(n):
     buf = [0]
     pos = 0
@@ -6,9 +5,22 @@ def step1(n):
         pos = (pos + n) % len(buf)
         pos += 1
         buf = buf[:pos] + [val] + buf[pos:]
-
     return buf[(pos + 1) % len(buf)]
+
+
+def step2(n):
+    pos = 0
+    buf_len = 1
+    # Python is really not the right language for these big loops
+    for val in range(1, int(50e6)):
+        pos = (pos + n) % buf_len
+        pos += 1
+        if pos == 1:
+            ans = val
+        buf_len += 1
+    return ans
         
 
 n = int(input())
 print(step1(n))
+print(step2(n))
