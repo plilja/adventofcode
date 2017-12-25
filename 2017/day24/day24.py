a928ea4cd36d748884197cf0975bf93bf466a7dd
Patrik Lilja <lilja.patrik@gmail.com>
2017-12-25 15:24:54 +0100

    Day 24 step 2

--- 2017/day24/day24.py
+++ 2017/day24/day24.py
@@ -24,6 +24,34 @@ def step1(inp):
     return f(components, 0, {})
 
 
+def step2(inp):
+    def f(components, connector, cache):
+        key = (components, connector)
+        if key in cache:
+            return cache[key]
+        else:
+            ans_len, ans_str = 0, 0
+            for c in components:
+                a, b = c
+                if a == connector or b == connector:
+                    if a == connector:
+                        sub_len, sub_str = f(components - {c}, b, cache)
+                    else:
+                        sub_len, sub_str = f(components - {c}, a, cache)
+                    sub_len += 1
+                    sub_str += a + b
+                    if sub_len > ans_len or (sub_len == ans_len and sub_str > ans_str):
+                        ans_len, ans_str = sub_len, sub_str
+            cache[key] = (ans_len, ans_str)
+            return (ans_len, ans_str)
+
+    components = frozenset()
+    for c in inp:
+        assert(c not in components)
+        components = components | {c}
+    return f(components, 0, {})[1]
+
+
 def get_input():
     res = []
     for s in sys.stdin:
@@ -33,4 +61,4 @@ def get_input():
 
 inp = get_input()
 print(step1(inp))
-
+print(step2(inp))
