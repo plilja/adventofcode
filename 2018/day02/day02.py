3fb90d5aa732a2ebd0d609affad1907cebc4a80f
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-02 10:01:02 +0100

    Day 2 step 2

--- 2018/day02/day02.py
+++ 2018/day02/day02.py
@@ -10,5 +10,18 @@ def step1(inp):
     return r[2] * r[3]
 
 
-inp = sys.stdin.readlines()
+def step2(inp):
+    def str_common(s1, s2):
+        return ''.join([a if a == b else '' for a, b in zip(s1, s2)])
+
+    for i in range(0, len(inp)):
+        for j in range(i + 1, len(inp)):
+            s = str_common(inp[i], inp[j])
+            if len(s) == len(inp[j]) - 1:
+                return s
+    raise ValueError('No solution found')
+
+
+inp = list(map(str.strip, sys.stdin.readlines()))
 print(step1(inp))
+print(step2(inp))
