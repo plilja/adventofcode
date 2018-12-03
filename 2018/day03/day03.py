26b22e063437574ad2b1652727596f033d6e7dce
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-03 18:49:55 +0100

    Day 3 step 2

--- 2018/day03/day03.py
+++ 2018/day03/day03.py
@@ -1,20 +1,41 @@
 import sys
-from collections import *
 import re
+from collections import *
 
 Claim = namedtuple('Claim', 'id left top width height')
+
+def claim_points(claim):
+    for y in range(0, claim.height):
+        for x in range(0, claim.width):
+            yield x, y
+
 
 def step1(claims):
     d = defaultdict(lambda: defaultdict(int))
     r = 0
     for claim in claims:
-        for y in range(0, claim.height):
-            for x in range(0, claim.width):
-                c = d[claim.top + y][claim.left + x]
-                d[claim.top + y][claim.left + x] += 1
-                if c == 1:
-                    r += 1
+        for x, y in claim_points(claim):
+            c = d[claim.top + y][claim.left + x]
+            d[claim.top + y][claim.left + x] += 1
+            if c == 1:
+                r += 1
     return r
+    
+
+def step2(claims):
+    d = defaultdict(lambda: defaultdict(set))
+    for claim in claims:
+        for x, y in claim_points(claim):
+            d[claim.top + y][claim.left + x] |= {claim.id}
+    for claim in claims:
+        poss = True
+        for x, y in claim_points(claim):
+            if d[claim.top + y][claim.left + x] != {claim.id}:
+                poss = False
+                break
+        if poss:
+            return claim.id
+    raise ValueError('No solution found')
     
 
 def read_claims():
@@ -30,3 +51,4 @@ def read_claims():
 
 claims = read_claims()
 print(step1(claims))
+print(step2(claims))
