85a08d5143afd73500dc81b26446159530aea4c7
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-06 21:30:03 +0100

    Day 6 step 2

--- 2018/day06/day06.py
+++ 2018/day06/day06.py
@@ -49,5 +49,26 @@ def step1(coordinates):
     return ans
 
 
+def step2(coordinates):
+    x1 = min(map(lambda p: p[0], coordinates))
+    x2 = max(map(lambda p: p[0], coordinates))
+    y1 = min(map(lambda p: p[1], coordinates))
+    y2 = max(map(lambda p: p[1], coordinates))
+    ans = 0
+    for a in range(x1, x2 + 1):
+        for b in range(y1, y2 + 1):
+            total_dist = 0
+            in_region = True
+            for x, y in coordinates:
+                total_dist += abs(a - x) + abs(b - y)
+                if total_dist >= 10000:
+                    in_region = False
+                    break
+            if in_region:
+                ans += 1
+    return ans
+
+
 coordinates = [tuple(map(int, s.split(', '))) for s in sys.stdin.readlines()]
 print(step1(coordinates))
+print(step2(coordinates))
