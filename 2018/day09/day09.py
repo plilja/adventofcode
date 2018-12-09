9f06046cacceb2ccc33be53a6f78a507af8133e5
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-09 09:52:01 +0100

    Day 9 step 1

---
+++ 2018/day09/day09.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def step1(num_players, last_marble):
+    players = [0] * num_players
+    circle = deque([0, 2, 1])
+    current_player = 2
+    # Always keep current marble one left of rightmost marble in circle
+    for marble in range(3, last_marble + 1):
+        if marble % 23 == 0:
+            for _ in range(0, 8):
+                circle.appendleft(circle.pop())
+            tmp = circle.pop()
+            players[current_player] += tmp
+            players[current_player] += marble
+            for _ in range(0, 2):
+                circle.append(circle.popleft())
+        else:
+            circle.append(marble)
+            circle.append(circle.popleft())
+        current_player = (current_player + 1) % num_players
+    return max(players)
+
+
+inp = input().split()
+num_players, last_marble = int(inp[0]), int(inp[6])
+print(step1(num_players, last_marble))
