f4d2e65caeeb5ce6176fe4bb9750f5cf13a4c892
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-11 21:27:28 +0100

    Day 11 step 2

--- 2018/day11/day11.py
+++ 2018/day11/day11.py
@@ -5,21 +5,37 @@ def power_level(serial_number, x, y):
 
 
 def step1(serial_number):
-    r = 0
-    rx = 0
-    ry = 0
-    for x in range(1, 301 - 3):
-        for y in range(1, 301 - 3):
+    x, y, _ = solve(serial_number, 3, 3)
+    return (x, y)
+
+
+def step2(serial_number):
+    return solve(serial_number, 1, 300)
+
+
+def solve(serial_number, min_size, max_size):
+    # grid[y][x] stores the sum of the grid bounded by (1, 1) to (x, y)
+    grid = [[0 for j in range(0, 302)] for i in range(0, 302)]
+    for y in range(1, 301):
+        s = 0
+        for x in range(1, 301):
+            s += power_level(serial_number, x, y)
+            grid[y][x] = grid[y - 1][x] + s
+
+    r, rz, ry, rz = 0, 0, 0, 0
+    for z in range(min_size, max_size + 1):
+        for y in range(1, 301 - z):
             s = 0
-            for i in range(0, 3):
-                for j in range(0, 3):
-                    s += power_level(serial_number, x + i, y + j)
-            if s > r:
-                r = s
-                rx = x
-                ry = y
-    return (rx, ry)
+            for x in range(1, 301 - z):
+                s = grid[y + z - 1][x + z - 1] - grid[y + z - 1][x - 1] - grid[y - 1][x + z - 1] + grid[y - 1][x - 1]
+                if s > r:
+                    r = s
+                    rx = x
+                    ry = y
+                    rz = z
+    return (rx, ry, rz)
 
 
 serial_number = int(input())
 print(step1(serial_number))
+print(step2(serial_number)) 
