3c66c6891b182217c5a6ad8cfe54a790b1477d74
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-12 18:11:05 +0100

    Day 12 step 2

--- 2018/day12/day12.py
+++ 2018/day12/day12.py
@@ -2,26 +2,48 @@ import sys
 from collections import defaultdict
 
 def step1(initial_state, notes):
+    return solve(initial_state, notes, 20)
+
+
+def step2(initial_state, notes):
+    return solve(initial_state, notes, 50000000000)
+
+
+def solve(initial_state, notes, iterations):
     assert '......' not in notes
     state = defaultdict(lambda: '.')
-    min_pot = 0
-    max_pot = len(initial_state)
-    for i in range(0, len(initial_state)):
-        state[i] = initial_state[i]
-    for i in range(0, 20):
-        next_state = defaultdict(lambda: '.')
-        for j in range(min_pot - 2, max_pot + 3):
-            key = ''.join([state[j + k] for k in range(-2, 3)])
-            next_state[j] = notes.get(key, '.')
-        state = next_state
-        for k in state.keys():
-            if state[k] == '#':
-                min_pot = min(min_pot, k)
-                max_pot = max(max_pot, k)
-    return sum([k for k in state.keys() if state[k] == '#'])
+    for i, v in enumerate(initial_state):
+        state[i] = v
+    # Store seen plant generations in cache normalized so that the leftmost plant
+    # is at index 0. 
+    cache = {}
+    generation = 0
+    # Off stores how much plants have been shifted to normalize plats at index 0
+    off = 0
+    while generation < iterations:
+        tmp = {}
+        for j in range(-2, max(state.keys()) + 3):
+            key = ''.join([state.get(j + k, '.') for k in range(-2, 3)])
+            tmp[j] = notes.get(key, '.')
+        m = min([k for k, v in tmp.items() if v == '#'])
+        off += m
+        next_state = {k - m:v for k, v in tmp.items() if v == '#'}
+        cache_key = tuple(next_state.keys())
+        generation += 1
+        if cache_key in cache:
+            (prev_gen, prev_off) = cache[cache_key]
+            d = generation - prev_gen
+            loops = (iterations - i - 1) // d
+            generation += d * loops
+            off += loops * (off - prev_off)
 
+        cache[cache_key] = (generation, off)
+        state = next_state
+
+    return sum([k + off for k in state.keys() if state[k] == '#'])
 
 state = input().split()[2]
 input() # skip blank row
 notes = {k:v for k, _, v in map(str.split, sys.stdin.readlines())}
 print(step1(state, notes))
+print(step2(state, notes))
