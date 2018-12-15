af12986a5c830bd0c635be6e722a981a96de136a
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-15 10:40:18 +0100

    Day 14 step 2

--- 2018/day14/day14.py
+++ 2018/day14/day14.py
@@ -1,10 +1,38 @@
 def step1(n):
     recipes = [3, 7]
-    elves = [0, 1]
+    e1, e2 = 0, 1
     while len(recipes) < n + 10:
-        recipes += [int(j) for j in str(sum(map(lambda i: recipes[i], elves)))]
-        elves = [(1 + elve + recipes[elve]) % len(recipes) for elve in elves]
+        r1 = recipes[e1]
+        r2 = recipes[e2]
+        r = r1 + r2
+        if r >= 10:
+            recipes.append(r // 10)
+        recipes.append(r % 10)
+        e1 = (e1 + r1 + 1) % len(recipes)
+        e2 = (e2 + r2 + 1) % len(recipes)
     return ''.join(map(str, recipes[n:n+10]))
+
+
+def step2(n):
+    key = [int(i) for i in str(n)]
+    recipes = [3, 7]
+    e1, e2 = 0, 1
+    while True:
+        r1 = recipes[e1]
+        r2 = recipes[e2]
+        r = r1 + r2
+        if r >= 10:
+            recipes.append(r // 10)
+            if recipes[-len(key):] == key:
+                break
+        recipes.append(r % 10)
+        if recipes[-len(key):] == key:
+            break
+        e1 = (e1 + r1 + 1) % len(recipes)
+        e2 = (e2 + r2 + 1) % len(recipes)
+    return len(recipes) - len(key)
+
 
 n = int(input())
 print(step1(n))
+print(step2(n)) # This one is slow
