54431f0ea8d7df862484f6e6ffcb9c6885881a97
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-22 21:56:45 +0100

    Day 21 step 2

--- 2018/day21/day21.py
+++ 2018/day21/day21.py
@@ -1,4 +1,5 @@
 import sys
+from math import *
 
 def opr(f):
     def op(registers, args):
@@ -49,9 +50,9 @@ ops = {
 def step1(ip, instructions):
     registers = [0, 0, 0, 0, 0, 0]
     while True:
-        if registers[ip] == 30:
+        if registers[ip] == 29:
             # If register 0 is the same as whatever register 4
-            # is when we reach line 30, ten the program ends.
+            # is when we reach line 29, ten the program ends.
             # This is the only usage of register 0
             return registers[4] 
         instruction = instructions[registers[ip]]
@@ -60,6 +61,34 @@ def step1(ip, instructions):
         if registers[ip] + 1 >= len(instructions):
             break
         registers[ip] += 1
+
+
+def step2(ip, instructions):
+    seen = set()
+    reg3, reg4, reg5 = 0, 0, 0
+    start_from_top = True
+    while True:
+        if start_from_top:
+            reg3 = reg4 | 65536
+            reg4 = 10552971
+
+        reg5 = reg3 & 255
+        reg4 += reg5
+        reg4 &= 16777215
+        reg4 *= 65899
+        reg4 &= 16777215
+        if 256 > reg3:
+            reg5 = 1
+            if reg4 in seen:
+                return last_seen
+            last_seen = reg4
+            seen.add(reg4)
+            start_from_top = True
+        else:
+            # Loop between 18 and 25 ends when (reg5 + 1) * 256 > reg3
+            reg5 = ceil((reg3 + 0.5) / 256) - 1
+            reg3 = reg5
+            start_from_top = False
 
 
 def parse_input():
@@ -76,3 +105,4 @@ def parse_input():
 
 ip, instructions = parse_input()
 print(step1(ip, instructions))
+print(step2(ip, instructions))
