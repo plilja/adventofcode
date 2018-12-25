519b262b35161d8f9b3f467713b1c93af0ca424f
Patrik Lilja <lilja.patrik@gmail.com>
2018-12-25 19:58:53 +0100

    Day 25

---
+++ 2018/day25/day25.py
@@ -0,0 +1,36 @@
+import sys
+from math import *
+from collections import deque, defaultdict
+
+def solve(points):
+    g = defaultdict(list)
+    for i in range(len(points)):
+        p1 = points[i]
+        for j in range(i + 1, len(points)):
+            p2 = points[j]
+            d = 0
+            for k in range(0, len(p1)):
+                d += abs(p1[k] - p2[k])
+            if d <= 3:
+                g[i].append(j)
+                g[j].append(i)
+
+    v = set()
+    r = 0
+    for i in range(len(points)):
+        if i in v:
+            continue
+        r += 1
+        q = deque([i])
+        while q:
+            j = q.popleft()
+            if j in v:
+                continue
+            v.add(j)
+            for k in g[j]:
+                q.append(k)
+    return r
+
+points = [tuple(map(int, s.strip().split(','))) for s in sys.stdin]
+print(solve(points))
+# No step 2 this day
