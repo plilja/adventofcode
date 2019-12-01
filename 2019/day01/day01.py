400b690d1752ea15a065ed4d251aef2a9b5ba6f4
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-01 09:53:10 +0100

    Day 1 step 2

--- 2019/day01/day01.py
+++ 2019/day01/day01.py
@@ -5,5 +5,17 @@ def step1(inp):
     return sum([x // 3 - 2 for x in inp])
 
 
+def step2(inp):
+    ans = 0
+    for x in inp:
+        mass = x
+        while mass > 0:
+            fuel = max(mass // 3 - 2, 0)
+            mass = fuel
+            ans += fuel
+    return ans
+
+
 inp = list(map(int, sys.stdin.readlines()))
 print(step1(inp))
+print(step2(inp))
