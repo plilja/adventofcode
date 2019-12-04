5f27d3054e8746e280c33fa82f9163cf9a10dd54
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-04 19:30:27 +0100

    Day 4 step 2

--- 2019/day04/day04.py
+++ 2019/day04/day04.py
@@ -1,9 +1,35 @@
+def group(ls):
+    """ Groups adjacent equal elements together,
+    for example [1, 1, 2, 3, 3] becomes [[1, 1], [2], [3, 3]]
+    """
+    res = []
+    prev = None
+    for a in ls:
+        if a != prev:
+            res.append([a])
+        else:
+            res[-1].append(a)
+        prev = a
+    return res
+
+
 def step1(start, end):
     res = 0
     for i in range(start, end + 1):
         s = str(i)
         a = len(s) == 6
-        b = any(map(lambda t: t[0] == t[1], zip(s, s[1:])))
+        b = any(map(lambda x: len(x) >= 2, group(s)))
+        c = all(map(lambda t: t[0] <= t[1], zip(s, s[1:])))
+        res += 1 if a and b and c else 0
+    return res
+
+
+def step2(start, end):
+    res = 0
+    for i in range(start, end + 1):
+        s = str(i)
+        a = len(s) == 6
+        b = any(map(lambda x: len(x) == 2, group(s)))
         c = all(map(lambda t: t[0] <= t[1], zip(s, s[1:])))
         res += 1 if a and b and c else 0
     return res
@@ -11,3 +37,4 @@ def step1(start, end):
 
 [start, end] = list(map(int, input().split()))
 print(step1(start, end))
+print(step2(start, end))
