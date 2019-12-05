0b2cc9d8c9c93515d85dfdb810daca597d01ea4b
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-05 19:59:17 +0100

    Day 5 step 2

--- 2019/day05/day05.py
+++ 2019/day05/day05.py
@@ -1,4 +1,4 @@
-def step1(program):
+def run_program(program, first_input):
 
     def read(arg, mode):
         if mode == '0':
@@ -12,11 +12,11 @@ def step1(program):
 
     last_output = None
     position = 0
-    while program[position] != 99:
+    while True:
         arg = ('0000' + str(program[position]))[-5:]
-        if arg == '00099':
-            break
         op = arg[3:5]
+        if op == '99':
+            break
         m1 = arg[2]
         m2 = arg[1]
         m3 = arg[0]
@@ -29,14 +29,43 @@ def step1(program):
             write(position + 3, m3, value)
             position += 4
         elif op == '03':
-            write(position + 1, m1, 1)
+            write(position + 1, m1, first_input)
             position += 2
-        else:
-            assert op == '04'
+        elif op == '04':
             last_output = read(position + 1, m1)
             position += 2
+        elif op == '05':
+            value = read(position + 1, m1)
+            if value != 0:
+                position = read(position + 2, m2)
+            else:
+                position += 3
+        elif op == '06':
+            value = read(position + 1, m1)
+            if value == 0:
+                position = read(position + 2, m2)
+            else:
+                position += 3
+        elif op == '07':
+            value = 1 if read(position + 1, m1) < read(position + 2, m2) else 0
+            write(position + 3, m3, value)
+            position += 4
+        else:
+            assert op == '08'
+            value = 1 if read(position + 1, m1) == read(position + 2, m2) else 0
+            write(position + 3, m3, value)
+            position += 4
     return last_output
+
+
+def step1(program):
+    return run_program(program[::], 1)
+
+
+def step2(program):
+    return run_program(program[::], 5)
 
 
 program = list(map(int, input().split(',')))
 print(step1(program))
+print(step2(program))
