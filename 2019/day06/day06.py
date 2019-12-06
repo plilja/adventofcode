aabf2408139570f0aead4799303823b7538908ad
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-06 17:20:20 +0100

    Day 6 step 2

--- 2019/day06/day06.py
+++ 2019/day06/day06.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def step1(inp):
@@ -20,5 +21,25 @@ def step1(inp):
     return sum([orbits(node) for node in g])
 
 
+def step2(inp):
+    g = defaultdict(list)
+    for a, b in inp:
+        g[b].append(a)
+        g[a].append(b)
+
+    q = ['YOU']
+    dist = {'YOU': 0}
+    while 'SAN' not in dist:
+        n = q[0]
+        q = q[1:]
+        d = dist[n]
+        for n2 in g[n]:
+            if n2 not in dist:
+                dist[n2] = d + 1
+                q.append(n2)
+    return dist['SAN'] - 2
+
+
 inp = [tuple(x.strip().split(')')) for x in sys.stdin]
 print(step1(inp))
+print(step2(inp))
