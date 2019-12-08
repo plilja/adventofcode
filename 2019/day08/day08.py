9f038ae0dda9eb3141227e61a9a80efaa22fa7ce
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-08 09:08:01 +0100

    Day 8 step 2

--- 2019/day08/day08.py
+++ 2019/day08/day08.py
@@ -15,5 +15,22 @@ def step1(inp):
     return res
 
 
+def step2(inp):
+    w = 25
+    h = 6
+    pixel_map = {'0': ' ', '1': '*', '2': '-'}
+    image = [['-'] * w for _ in range(0, h)]
+    size = w * h
+    for i in range(0, len(inp) // size):
+        layer = inp[i * size:(i + 1) * size]
+        for y in range(0, h):
+            for x in range(0, w):
+                image_pixel = image[y][x]
+                if image_pixel == '-':
+                    image[y][x] = pixel_map[layer[y * w + x]]
+    return '\n'.join([''.join(x) for x in image])
+
+
 inp = input()
 print(step1(inp))
+print(step2(inp))
