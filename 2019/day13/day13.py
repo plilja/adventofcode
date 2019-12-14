69838aaf8544b98c1c23b1c03ab30946a4bd5349
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-14 13:06:12 +0100

    Day 13 step 2

--- 2019/day13/day13.py
+++ 2019/day13/day13.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+TILE_TYPES = {0: ' ', 1: '#', 2: '^', 3: '-', 4: 'o'}
+
 class Process:
     def __init__(self, inp):
         self.position = 0
@@ -118,5 +120,46 @@ def step1(program):
     return result
 
 
+
+def find(grid, what):
+    for y in grid.keys():
+        for x in grid[y].keys():
+            if grid[y][x] == what:
+                return (x, y)
+
+
+def step2(program):
+    program_copy = program[::]
+    program_copy[0] = 2
+    process = Process(program_copy)
+    grid = defaultdict(lambda: defaultdict(int))
+    score = None
+    while process.is_running():
+        while process.is_running() and not process.needs_input() and len(process.output) < 3:
+            process.tick()
+
+        if len(process.output) >= 3:
+            x = process.pop_output()
+            y = process.pop_output()
+            tile = process.pop_output()
+            if (-1, 0) == (x, y): 
+                score = tile
+            else:
+                grid[y][x] = tile
+
+        if process.needs_input():
+            # Keep paddle under ball at all times
+            bx, _ = find(grid, 4)
+            px, _ = find(grid, 3)
+            if bx < px:
+                process.add_input(-1)
+            elif px == bx:
+                process.add_input(0)
+            else:
+                process.add_input(1)
+    return score
+
+
 program = list(map(int, input().split(',')))
 print(step1(program))
+print(step2(program))
