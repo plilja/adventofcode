8cc495a349cf035252b5636ec4bcf1e0b1f5f8fb
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-24 15:04:00 +0100

    Day 16 step 2

--- 2019/day16/day16.py
+++ 2019/day16/day16.py
@@ -1,6 +1,5 @@
 BASE_PATTERN = [0, 1, 0, -1]
 
-
 def step1(signal):
     r = [int(t) for t in signal]
     for phase in range(0, 100):
@@ -16,5 +15,24 @@ def step1(signal):
     return ''.join(list(map(str, r)))[0:8]
 
 
+def step2(inp_signal):
+    offset = int(inp_signal[0:7])
+    r = list(map(int, reversed((inp_signal * 10000)[offset:])))
+    # The utilizes the fact that the offset is large and ends 
+    # up being in the portion of the signal where every number is
+    # always multiplied by -1 (or 1 if you only consider the positive
+    # numbers). This solution wouldn't work if the offset was smaller.
+    for phase in range(0, 100):
+        acc = 0
+        next_r = []
+        for i in range(0, len(r)):
+            acc = (acc + r[i]) % 10
+            next_r.append(acc)
+        r = next_r
+    r = reversed(r[-8:])
+    return ''.join(list(map(str, r)))
+
+
 signal = input()
 print(step1(signal))
+print(step2(signal))
