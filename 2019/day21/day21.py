bd4faf26f20ca145038120dc4b51ffed864a083b
Patrik Lilja <lilja.patrik@gmail.com>
2019-12-28 11:16:13 +0100

    Day 21 step 2

--- 2019/day21/day21.py
+++ 2019/day21/day21.py
@@ -24,5 +24,29 @@ def step1(instructions):
     return process.output[-1]
 
 
+def step2(instructions):
+    process = IntcodeProcess(instructions)
+    #  ABCDEFGHI
+    ###  ## ### 
+    enter(process, 'NOT C T')
+    enter(process, 'NOT B J')
+    enter(process, 'OR T J')
+    enter(process, 'NOT A T')
+    enter(process, 'OR T J')  # Do we have a reason to jump?
+
+    enter(process, 'OR J T')
+    enter(process, 'NOT T T')  # Set T to false
+
+    enter(process, 'OR E T')
+    enter(process, 'OR H T')
+    enter(process, 'AND D T')  # We can jump to D and continue to either E or I?
+
+    enter(process, 'AND T J')  # Do we have a reason to jump and can continue after jump?
+    enter(process, 'RUN')
+    process.run_until_complete()
+    return process.output[-1]
+
+
 instructions = list(map(int, input().split(',')))
 print(step1(instructions))
+print(step2(instructions))
