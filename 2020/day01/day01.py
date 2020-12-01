abc2112d0f93a6a4caf437f7b373070603326dc3
Patrik Lilja <lilja.patrik@gmail.com>
2020-12-01 19:50:05 +0100

    Day 1 step 2

--- 2020/day01/day01.py
+++ 2020/day01/day01.py
@@ -13,5 +13,24 @@ def step1(nums):
     raise ValueError('Unable to find solution')
 
 
+def step2(nums):
+    counts = Counter(nums)
+    for num1 in sorted(counts.keys()):
+        for num2 in sorted(counts.keys()):
+            if num2 > num1:
+                break
+            if num1 == num2 and counts[num1] < 2:
+                continue
+            rem = 2020 - num1 - num2
+            if num1 == num2 and num2 == rem and counts[rem] >= 3:
+                return num1 * num2 * rem
+            elif num1 == rem or num2 == rem and counts[rem] >= 2:
+                return num1 * num2 * rem
+            elif counts[rem] >= 1:
+                return num1 * num2 * rem
+    raise ValueError('Unable to find solution')
+
+
 nums = list(map(int, sys.stdin.readlines()))
 print(step1(nums))
+print(step2(nums))
