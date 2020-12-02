3d04e60eb277f946ca198ce24db14b757465c3f0
Patrik Lilja <lilja.patrik@gmail.com>
2020-12-02 19:32:02 +0100

    Day 2 step 2

--- 2020/day02/day02.py
+++ 2020/day02/day02.py
@@ -2,14 +2,14 @@ import re
 import sys
 from collections import namedtuple, Counter
 
-Pattern = namedtuple('Pattern', 'minimum maximum char')
+Pattern = namedtuple('Pattern', 'a b char')
 
 
 def parse_input(inp):
     result = []
     for line in inp:
-        [minimum, maximum, char, password] = re.match(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', line).groups()
-        result += [(Pattern(int(minimum), int(maximum), char), password)]
+        [a, b, char, password] = re.match(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', line).groups()
+        result += [(Pattern(int(a), int(b), char), password)]
     return result
 
 
@@ -17,10 +17,21 @@ def step1(inp):
     result = 0
     for pattern, password in parse_input(inp):
         counter = Counter(password)
-        if pattern.minimum <= counter[pattern.char] and pattern.maximum >= counter[pattern.char]:
+        if pattern.a <= counter[pattern.char] and pattern.b >= counter[pattern.char]:
+            result += 1
+    return result
+
+
+def step2(inp):
+    result = 0
+    for pattern, password in parse_input(inp):
+        x = password[pattern.a - 1] == pattern.char
+        y = password[pattern.b - 1] == pattern.char
+        if x != y:
             result += 1
     return result
 
 
 inp = sys.stdin.readlines()
 print(step1(inp))
+print(step2(inp))
