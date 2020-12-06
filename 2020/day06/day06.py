79dc78be4d35bdad2ac5a33b5147020a83c0855e
Patrik Lilja <lilja.patrik@gmail.com>
2020-12-06 08:44:43 +0100

    Day 6 step 2

--- 2020/day06/day06.py
+++ 2020/day06/day06.py
@@ -2,18 +2,36 @@ import sys
 from collections import Counter
 
 
-def step1(inp):
-    result = 0
-    group = Counter()
+def divide_groups(inp):
+    result = [[]]
     for line in inp:
         if not line:
-            result += len(group)
-            group = Counter()
+            result.append([])
         else:
-            group += Counter(line)
-    result += len(group)
+            result[-1].append(line)
+    return result
+
+
+def step1(inp):
+    result = 0
+    for group in divide_groups(inp):
+        group_counter = Counter()
+        for line in group:
+            group_counter += Counter(line)
+        result += len(group_counter)
+    return result
+
+
+def step2(inp):
+    result = 0
+    for group in divide_groups(inp):
+        distinct_answers = set([c for c in group[0]])
+        for line in group[1:]:
+            distinct_answers = distinct_answers & set([c for c in line])
+        result += len(distinct_answers)
     return result
 
 
 inp = [line.strip() for line in sys.stdin.readlines()]
 print(step1(inp))
+print(step2(inp))
