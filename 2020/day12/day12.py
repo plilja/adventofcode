b85974604d5706f942f6c3a2d37a86eba94e0c0d
Patrik Lilja <lilja.patrik@gmail.com>
2020-12-12 10:17:16 +0100

    Day 12 step 2

--- 2020/day12/day12.py
+++ 2020/day12/day12.py
@@ -28,5 +28,33 @@ def step1(instructions):
     return abs(x) + abs(y)
 
 
+def step2(instructions):
+    x, y = 0, 0
+    dx, dy = 10, -1  # starting position from problem description
+    for instruction in instructions:
+        d = instruction[0]
+        n = int(instruction[1:])
+        if d == 'F':
+            x += dx * n
+            y += dy * n
+        elif d == 'N':
+            dy -= n  # unintuitively north means decreasing y
+        elif d == 'S':
+            dy += n
+        elif d == 'E':
+            dx += n
+        elif d == 'W':
+            dx -= n
+        elif d == 'L':
+            for i in range(0, n // 90):
+                dx, dy = dy, -dx
+        else:
+            assert d == 'R'
+            for i in range(0, n // 90):
+                dx, dy = -dy, dx
+    return abs(x) + abs(y)
+
+
 instructions = [x.strip() for x in sys.stdin]
 print(step1(instructions))
+print(step2(instructions))
