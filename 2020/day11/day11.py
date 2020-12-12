3b19f4d40cc35555e9822e5aba61f4ea839343d5
Patrik Lilja <lilja.patrik@gmail.com>
2020-12-12 09:45:27 +0100

    Day 11 step 2

--- 2020/day11/day11.py
+++ 2020/day11/day11.py
@@ -15,7 +15,7 @@ EMPTY = 'L'
 OCCUPIED = '#'
 
 
-def safe_get(seats, x, y):
+def safe_get(seats, x, dx, y, dy):
     if y < 0 or y >= len(seats):
         return FLOOR
     if x < 0 or x >= len(seats[y]):
@@ -23,7 +23,19 @@ def safe_get(seats, x, y):
     return seats[y][x]
 
 
-def step1(inp):
+def get_diagonal(seats, x, dx, y, dy):
+    while y >= 0 and y < len(seats) and \
+            x >= 0 and x < len(seats[0]):
+        seat = seats[y][x]
+        if seat != FLOOR:
+            return seat
+        else:
+            x += dx
+            y += dy
+    return FLOOR
+
+
+def solve(inp, nearby_seat_func, limit):
     seats = [copy.deepcopy(inp), copy.deepcopy(inp)]
     i = 0
     while True:
@@ -33,13 +45,13 @@ def step1(inp):
                 seat = seats[i % 2][y][x]
                 in_proximity = 0
                 for dx, dy in DELTAS:
-                    if safe_get(seats[i % 2], x + dx, y + dy) == OCCUPIED:
+                    if nearby_seat_func(seats[i % 2], x + dx, dx, y + dy, dy) == OCCUPIED:
                         in_proximity += 1
 
                 if seat == EMPTY and in_proximity == 0:
                     seats[(i + 1) % 2][y][x] = OCCUPIED
                     changes += 1
-                elif seat == OCCUPIED and in_proximity >= 4:
+                elif seat == OCCUPIED and in_proximity >= limit:
                     seats[(i + 1) % 2][y][x] = EMPTY
                     changes += 1
                 else:
@@ -56,5 +68,14 @@ def step1(inp):
     return result
 
 
+def step1(inp):
+    return solve(inp, safe_get, 4)
+
+
+def step2(inp):
+    return solve(inp, get_diagonal, 5)
+
+
 inp = [list(x.strip()) for x in sys.stdin]
 print(step1(inp))
+print(step2(inp))
