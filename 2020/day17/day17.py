d6383b1b535eabbb28ba7bee25560d3af75f7155
Patrik Lilja <lilja.patrik@gmail.com>
2020-12-18 17:35:23 +0100

    Day 17 step 2

--- 2020/day17/day17.py
+++ 2020/day17/day17.py
@@ -1,29 +1,45 @@
 import sys
 from collections import defaultdict
 
+DELTAS = [[] for i in range(0, 7)]
 
-def neighbours(point):
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            for dz in range(-1, 2):
-                if dx != 0 or dy != 0 or dz != 0:
-                    yield (point[0] + dx, point[1] + dy, point[2] + dz)
+for i in range(1, 6):
+    if i == 1:
+        DELTAS[i] = [(-1,), (0,), (1,)]
+    else:
+        DELTAS[i] += [x + (-1,) for x in DELTAS[i - 1]]
+        DELTAS[i] += [x + (0,) for x in DELTAS[i - 1]]
+        DELTAS[i] += [x + (1,) for x in DELTAS[i - 1]]
 
 
-def step1(initial_state):
+def neighbours(point, i=0):
+    for deltas in DELTAS[len(point)]:
+        xs = []
+        for i in range(0, len(point)):
+            xs.append(point[i] + deltas[i])
+        yield tuple(xs)
+
+
+def run(initial_state, dim):
+    neighbours_cache = {}
     space = [defaultdict(int), defaultdict(int)]
     for y, xs in enumerate(initial_state):
         for x, c in enumerate(xs):
-            space[0][(x, y, 0)] = 1 if c == '#' else 0
+            key = (x, y) + tuple(0 for i in range(0, dim - 2))
+            space[0][key] = 1 if c == '#' else 0
     for i in range(1, 8):
         nodes = set()
         for point in space[(i - 1) % 2].keys():
             for neigh in neighbours(point):
                 nodes.add(neigh)
-                nodes.add(point)
         for node in nodes:
             state = space[(i - 1) % 2][node]
-            s = sum([space[(i - 1) % 2][neigh] for neigh in neighbours(node)])
+            neighs = neighbours_cache.get(node, None)
+            if not neighs:
+                neighs = list(neighbours(node))
+                neighbours_cache[node] = neighs
+            s = sum([space[(i - 1) % 2][neigh] for neigh in neighs])
+            s -= state
             if state == 1:
                 if 2 <= s <= 3:
                     new_state = 1
@@ -38,5 +54,15 @@ def step1(initial_state):
     return sum([x for x in space[8 % 2].values()])
 
 
+def step1(initial_state):
+    return run(initial_state, 3)
+
+
+def step2(initial_state):
+    return run(initial_state, 4)
+
+
 initial_state = [x.strip() for x in sys.stdin]
 print(step1(initial_state))
+print(step2(initial_state))
+# this one is really slow
