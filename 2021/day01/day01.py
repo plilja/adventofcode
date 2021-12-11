340a0b6e0937900e35e704fcbf037586d42f3376
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-11 15:13:19 +0100

    Day 1 step 1

---
+++ 2021/day01/day01.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def step1(inp):
+    result = 0
+    for x, y in zip(inp, inp[1:]):
+        if x < y:
+            result += 1
+    return result
+
+
+inp = [int(i) for i in sys.stdin]
+print(step1(inp))
