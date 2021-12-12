386ccf140b6af4f88bc5be92f1cea5f133fdc5b3
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-12 09:41:36 +0100

    Day 2 step 2

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -15,5 +15,22 @@ def step1(inp):
     return horizontal * depth
 
 
+def step2(inp):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for line in inp:
+        command, i = line.split(' ')
+        if command == 'forward':
+            horizontal += int(i)
+            depth += aim * int(i)
+        if command == 'down':
+            aim += int(i)
+        if command == 'up':
+            aim -= int(i)
+    return horizontal * depth
+
+
 inp = sys.stdin.readlines()
 print(step1(inp))
+print(step2(inp))
