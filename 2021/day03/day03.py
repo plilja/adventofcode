56cfca86b92fe66a867ada42983b481196819c81
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-13 19:21:17 +0100

    Day 3 step 2

--- 2021/day03/day03.py
+++ 2021/day03/day03.py
@@ -14,5 +14,34 @@ def step1(inp):
     return int(gamma, 2) * int(epsilon, 2)
 
 
+def step2(inp):
+    n = len(inp[0])
+
+    oxygen = inp[::]
+    for i in range(0, n):
+        c = Counter(map(lambda s: s[i], oxygen))
+        common = c.most_common()
+        k = common[0][0]
+        if common[0][1] == common[1][1]:
+            k = '1'  # tie, favor 1
+        oxygen = list(filter(lambda s: s[i] == k, oxygen))
+        if len(oxygen) == 1:
+            break
+
+    co2 = inp[::]
+    for i in range(0, n):
+        c = Counter(map(lambda s: s[i], co2))
+        common = c.most_common()
+        k = common[1][0]
+        if common[0][1] == common[1][1]:
+            k = '0'  # tie, favor 0
+        co2 = list(filter(lambda s: s[i] == k, co2))
+        if len(co2) == 1:
+            break
+
+    return int(oxygen[0], 2) * int(co2[0], 2)
+
+
 inp = [s.strip() for s in sys.stdin]
 print(step1(inp))
+print(step2(inp))
