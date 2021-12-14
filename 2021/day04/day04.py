91bf71b591176a0e0294773d448b423cca7516f1
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-14 19:47:22 +0100

    Day 4 step 2

--- 2021/day04/day04.py
+++ 2021/day04/day04.py
@@ -1,18 +1,23 @@
 import sys
 from collections import defaultdict
 
+coordinates = [(x, y) for y in range(0, 5) for x in range(0, 5)]
 
-def step1(draw, boards):
+
+def calculate_results(draw, boards):
     num_to_board = defaultdict(list)
     for board_num, board in enumerate(boards):
-        for y in range(0, 5):
-            for x in range(0, 5):
-                num_to_board[board[y][x]].append((board_num, x, y))
+        for x, y in coordinates:
+            num_to_board[board[y][x]].append((board_num, x, y))
     bingo = [defaultdict(lambda: defaultdict(int)) for i in range(0, len(boards))]
+    results = []
+    winners = set()
     for num in draw:
         for board_num, x, y in num_to_board[num]:
             bingo[board_num][y][x] = 1
         for board_num, board in enumerate(boards):
+            if board_num in winners:
+                continue
             is_bingo = False
             for a in range(0, 5):
                 s1 = sum(map(lambda b: bingo[board_num][a][b], range(0, 5)))
@@ -22,12 +27,20 @@ def step1(draw, boards):
 
             if is_bingo:
                 result = 0
-                for y in range(0, 5):
-                    for x in range(0, 5):
-                        if bingo[board_num][y][x] == 0:
-                            result += board[y][x]
-                return result * num
-    raise ValueError('No bingo')
+                for x, y in coordinates:
+                    if bingo[board_num][y][x] == 0:
+                        result += board[y][x]
+                winners.add(board_num)
+                results.append((board_num, result * num))
+    return results
+
+
+def step1(draw, boards):
+    return calculate_results(draw, boards)[0][1]
+
+
+def step2(draw, boards):
+    return calculate_results(draw, boards)[-1][1]
 
 
 def read_input():
@@ -43,3 +56,4 @@ def read_input():
 
 draw, boards = read_input()
 print(step1(draw, boards))
+print(step2(draw, boards))
