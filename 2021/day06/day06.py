cd147e28b429642e992c8d481682464db7b6fda3
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-16 18:15:13 +0100

    Day 6 step 2

--- 2021/day06/day06.py
+++ 2021/day06/day06.py
@@ -1,8 +1,16 @@
 def step1(fishes):
+    return calculate_fishes(fishes, 80)
+
+
+def step2(fishes):
+    return calculate_fishes(fishes, 256)
+
+
+def calculate_fishes(fishes, days):
     ages = {i: 0 for i in range(0, 9)}
     for age in fishes:
         ages[age] += 1
-    for i in range(0, 80):
+    for i in range(0, days):
         new_fishes = ages[0]
         for age in range(1, 9):
             ages[age - 1] = ages[age]
@@ -13,3 +21,4 @@ def step1(fishes):
 
 fishes = [int(i) for i in input().split(',')]
 print(step1(fishes))
+print(step2(fishes))
