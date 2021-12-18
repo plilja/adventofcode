80876b6e25f599d6d58221c0befe32b91e1caf57
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-18 10:39:45 +0100

    Day 7 step 2

--- 2021/day07/day07.py
+++ 2021/day07/day07.py
@@ -1,5 +1,3 @@
-
-
 def step1(crabs):
     result = float('inf')
     for pos in range(min(crabs), max(crabs) + 1):
@@ -10,5 +8,17 @@ def step1(crabs):
     return result
 
 
+def step2(crabs):
+    result = float('inf')
+    for pos in range(min(crabs), max(crabs) + 1):
+        cost = 0
+        for crab in crabs:
+            n = abs(crab - pos)
+            cost += int(n * (n + 1) / 2)
+        result = min(result, cost)
+    return result
+
+
 crabs = [int(i) for i in input().split(',')]
 print(step1(crabs))
+print(step2(crabs))
