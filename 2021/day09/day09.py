e166473fdbcc8b3263665b0726f8e9f0e88b5b9c
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-20 17:26:11 +0100

    Day 9 step 2

--- 2021/day09/day09.py
+++ 2021/day09/day09.py
@@ -16,5 +16,29 @@ def step1(inp):
     return result
 
 
+def step2(inp):
+    def dfs(x, y, visited):
+        if x < 0 or len(inp[0]) <= x or \
+                y < 0 or len(inp) <= y or \
+                (x, y) in visited or \
+                inp[y][x] == 9:
+            return 0
+        result = 1
+        visited.add((x, y))
+        for dx, dy in deltas:
+            result += dfs(x + dx, y + dy, visited)
+        return result
+
+    visited = set()
+    basins = []
+    for y in range(0, len(inp)):
+        for x in range(0, len(inp[y])):
+            basin = dfs(x, y, visited)
+            basins.append(basin)
+    basins.sort(key=lambda x: -x)
+    return basins[0] * basins[1] * basins[2]
+
+
 inp = [list(map(int, line.strip())) for line in sys.stdin]
 print(step1(inp))
+print(step2(inp))
