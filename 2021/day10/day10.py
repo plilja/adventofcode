1e4c726764f69461c58633424a896d1175a6b4db
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-22 11:04:44 +0100

    Day 10 step 2

--- 2021/day10/day10.py
+++ 2021/day10/day10.py
@@ -1,19 +1,19 @@
 import sys
 
-SCORES = {')': 3, ']': 57, '}': 1197, '>': 25137}
 CLOSING = {'(': ')', '[': ']', '{': '}', '<': '>'}
 
 
 def step1(inp):
+    scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
     result = 0
-    stack = []
     for line in inp:
+        stack = []
         for c in line:
             if c in CLOSING:
                 stack.append(CLOSING[c])
             elif c in CLOSING.values():
-                if stack[-1] != c:
-                    result += SCORES[c]
+                if not stack or stack[-1] != c:
+                    result += scores[c]
                     break
                 else:
                     stack.pop()
@@ -22,5 +22,33 @@ def step1(inp):
     return result
 
 
+def step2(inp):
+    scores = {')': 1, ']': 2, '}': 3, '>': 4}
+    rows = []
+    for line in inp:
+        valid = True
+        stack = []
+        for c in line:
+            if c in CLOSING:
+                stack.append(CLOSING[c])
+            elif c in CLOSING.values():
+                if not stack or stack[-1] != c:
+                    valid = False
+                    break  # Illegal, discard row
+                else:
+                    stack.pop()
+            else:
+                raise ValueError('Illegal character %s encountered' % c)
+        if valid:
+            score = 0
+            for c in stack[::-1]:
+                score = 5 * score + scores[c]
+            rows.append(score)
+
+    rows.sort()
+    return rows[len(rows) // 2]
+
+
 inp = [s.strip() for s in sys.stdin]
 print(step1(inp))
+print(step2(inp))
