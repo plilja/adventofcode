d639821fefef646fdc127bdf689acb46994545b3
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-23 09:23:29 +0100

    Day 11 step 1

---
+++ 2021/day11/day11.py
@@ -0,0 +1,36 @@
+import sys
+
+deltas = [(1, 0),
+          (1, 1),
+          (0, 1),
+          (-1, 1),
+          (-1, 0),
+          (-1, -1),
+          (0, -1),
+          (1, -1)]
+
+
+def step1(grid):
+    result = 0
+    for i in range(0, 100):
+        flashes = []
+        for y in range(0, 10):
+            for x in range(0, 10):
+                grid[y][x] += 1
+                if grid[y][x] == 10:
+                    flashes.append((x, y))
+        while flashes:
+            x, y = flashes.pop(0)
+            grid[y][x] = 0
+            result += 1
+            for dx, dy in deltas:
+                if 0 <= x + dx < 10 and 0 <= y + dy < 10:
+                    if 0 < grid[y + dy][x + dx] < 10:
+                        grid[y + dy][x + dx] += 1
+                        if grid[y + dy][x + dx] == 10:
+                            flashes.append((x + dx, y + dy))
+    return result
+
+
+grid = [list(map(int, s.strip())) for s in sys.stdin]
+print(step1(grid))
