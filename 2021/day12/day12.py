edcd895d1800434b5fc49a046cd5f7b4c7f356a2
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-24 09:30:34 +0100

    Day 12 step 2

--- 2021/day12/day12.py
+++ 2021/day12/day12.py
@@ -12,9 +12,31 @@ def step1(graph):
             result += 1
         else:
             for neighbour in graph[last]:
+                if neighbour == 'start':
+                    continue
                 if 'a' <= neighbour[0] <= 'z' and neighbour in path:
                     continue
                 q.append(path + [neighbour])
+    return result
+
+
+def step2(graph):
+    q = [(['start'], False)]
+    result = 0
+    while q:
+        path, small_used = q.pop()
+        last = path[-1]
+        if last == 'end':
+            result += 1
+        else:
+            for neighbour in graph[last]:
+                if neighbour == 'start':
+                    continue
+                if 'a' <= neighbour[0] <= 'z' and neighbour in path:
+                    if not small_used:
+                        q.append((path + [neighbour], True))
+                else:
+                    q.append((path + [neighbour], small_used))
     return result
 
 
@@ -29,3 +51,4 @@ def read_input():
 
 graph = read_input()
 print(step1(graph))
+print(step2(graph))
