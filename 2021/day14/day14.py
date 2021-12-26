6a64285f0c0174c6e6f59eae003fd221293e7271
Patrik Lilja <lilja.patrik@gmail.com>
2021-12-26 12:39:29 +0100

    Day 14 step 2

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -2,22 +2,50 @@ import sys
 from collections import Counter
 
 
-def step1(template, rules):
-    for i in range(0, 10):
-        new_template = [template[0]]
-        for x, y in zip(template, template[1:]):
-            key = x + y
-            if key in rules:
-                new_template.append(rules[key])
-            new_template.append(y)
-        template = new_template
-    counter = Counter(template).most_common()
-    return counter[0][1] - counter[-1][1]
+def step1(start_template, rules):
+    return run_process(start_template, rules, 10)
+
+
+def step2(start_template, rules):
+    return run_process(start_template, rules, 40)
+
+
+def run_process(start_template, rules, total_steps):
+    def helper(template, steps, cache):
+        key = (template, steps)
+        if key in cache:
+            return cache[key]
+        elif steps == 0:
+            result = Counter(template)
+        elif len(template) <= 3:
+            new_template = template[0]
+            for x, y in zip(template, template[1:]):
+                insert = rules.get(x + y, '')
+                new_template += insert
+                new_template += y
+            result = helper(new_template, steps - 1, cache)
+        else:
+            a = template[:len(template) // 2]
+            b = template[len(template) // 2:]
+            joint = a[-1] + b[0]
+            result = Counter()
+            insert = rules.get(joint)
+            if insert:
+                result += helper(joint, steps, cache)
+                result[joint[0]] -= 1
+                result[joint[1]] -= 1
+            result += helper(a, steps, cache)
+            result += helper(b, steps, cache)
+        cache[key] = result
+        return result
+
+    mc = helper(start_template, total_steps, {}).most_common()
+    return mc[0][1] - mc[-1][1]
 
 
 def read_input():
     inp = sys.stdin.readlines()
-    template = list(inp[0].strip())
+    template = inp[0].strip()
     rules = {}
     for line in inp[2:]:
         args = line.split()
@@ -27,3 +55,4 @@ def read_input():
 
 template, rules = read_input()
 print(step1(template, rules))
+print(step2(template, rules))
