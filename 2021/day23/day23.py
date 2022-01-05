a7ce4ac6093b4b1e4ed39d9b902d6f405c9e4edc
Patrik Lilja <lilja.patrik@gmail.com>
2022-01-05 10:47:51 +0100

    Day 23 step 2

--- 2021/day23/day23.py
+++ 2021/day23/day23.py
@@ -1,4 +1,5 @@
 import heapq
+import sys
 
 
 COST = {'A': 1,
@@ -11,31 +12,42 @@ CORRECT_X = {'A': 3,
              'C': 7,
              'D': 9}
 
-GRID = ['#############',
-        '#  . . . .  #',
-        '###.#.#.#.###',
-        '  #.#.#.#.#',
-        '  #########']
+GRID1 = ['#############',
+         '#  . . . .  #',
+         '###.#.#.#.###',
+         '  #.#.#.#.#',
+         '  #########']
+
+GRID2 = ['#############',
+         '#  . . . .  #',
+         '###.#.#.#.###',
+         '  #.#.#.#.#',
+         '  #.#.#.#.#',
+         '  #.#.#.#.#',
+         '  #########']
 
 DELTAS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
-def step1(grid):
-    return solve(grid)
+def step1(inp):
+    return solve(GRID1, inp)
 
 
-def solve(init_grid):
+def step2(inp):
+    return solve(GRID2, inp)
+
+
+def solve(grid, inp):
     incorrect_initially = set()
     correct_initially = set()
-    for y in range(len(init_grid) - 1, -1, -1):
-        for x in range(0, len(init_grid[y])):
-            c = init_grid[y][x]
+    for y in range(len(inp) - 1, -1, -1):
+        for x in range(0, len(inp[y])):
+            c = inp[y][x]
             if c in 'ABCD':
-                if is_final_target(correct_initially, x, y, c):
+                if is_final_target(grid, correct_initially, x, y, c):
                     correct_initially.add((x, y, c))
                 else:
                     incorrect_initially.add((x, y, c))
-
     pq = [(0, frozenset(correct_initially), frozenset(incorrect_initially))]
     result = float('inf')
     cache = {}
@@ -47,18 +59,18 @@ def solve(init_grid):
         if cache.get(key, float('inf')) <= cost:
             continue
         cache[key] = cost
-        if len(correct) == 8:
+        if len(incorrect) == 0:
             result = min(result, cost)
             continue
-        for fr, to, move_cost in possible_moves(correct, incorrect):
-            if is_final_target(correct, to[0], to[1], to[2]):
+        for fr, to, move_cost in possible_moves(grid, correct, incorrect):
+            if is_final_target(grid, correct, to[0], to[1], to[2]):
                 heapq.heappush(pq, (cost + move_cost, correct | {to}, incorrect - {fr}))
             else:
                 heapq.heappush(pq, (cost + move_cost, correct, (incorrect - {fr}) | {to}))
     return result
 
 
-def possible_moves(correct, incorrect):
+def possible_moves(grid, correct, incorrect):
     result = []
     occupied = set()
     for (x, y, c) in correct:
@@ -72,35 +84,38 @@ def possible_moves(correct, incorrect):
             move_cost, (x, y, c) = q.pop(0)
             vis.add((x, y))
             if init_pos != (x, y, c):
-                if is_final_target(correct, x, y, c):
+                if is_final_target(grid, correct, x, y, c):
                     result.append((init_pos, (x, y, c), move_cost))
-                elif init_pos[1] != 1 and GRID[y][x] == ' ':
+                elif init_pos[1] != 1 and grid[y][x] == ' ':
                     result.append((init_pos, (x, y, c), move_cost))  # can rest at this position
             for dx, dy in DELTAS:
                 new_pos = (x + dx, y + dy, c)
-                if GRID[y + dy][x + dx] != '#' and (x + dx, y + dy) not in vis and (x + dx, y + dy) not in occupied:
+                if grid[y + dy][x + dx] != '#' and (x + dx, y + dy) not in vis and (x + dx, y + dy) not in occupied:
                     vis.add((x, y))
                     q.append((move_cost + COST[c], new_pos))
     return result
 
 
-def is_final_target(correct, x, y, c):
+def is_final_target(grid, correct, x, y, c):
     assert c in 'ABCD'
     y2 = y + 1
     valid = True
-    while GRID[y2][x] != '#':
+    while grid[y2][x] != '#':
         valid = valid and (x, y2, c) in correct
         y2 += 1
     return valid and CORRECT_X[c] == x
 
 
 def read_input():
-    grid = []
+    inp = []
     for y in range(0, 5):
         row = input()
-        grid.append(tuple(row))
-    return tuple(grid)
+        inp.append(tuple(row))
+    return tuple(inp)
 
 
-grid = read_input()
-print(step1(grid))
+print('This one is slow. Wait for it...', file=sys.stderr)
+inp1 = read_input()
+print(step1(inp1))
+inp2 = tuple(list(inp1[:3]) + [tuple('  #D#C#B#A#'), tuple('  #D#B#A#C#')] + list(inp1[3:]))
+print(step2(inp2))
