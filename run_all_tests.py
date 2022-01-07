2515114b5458f8ebf076505f96ca1f49101742b0
Patrik Lilja <lilja.patrik@gmail.com>
2022-01-07 14:35:32 +0100

    Update test runner

--- run_all_tests.py
+++ run_all_tests.py
@@ -2,6 +2,7 @@
 
 import os
 
+
 def test(f):
     p = os.getcwd()
     os.chdir(f)
@@ -11,13 +12,20 @@ def test(f):
         os.system('~/workspace/algolib/util/checksol.py %s -e python3' % pys[0])
     os.chdir(p)
 
+
 def run():
     print(os.getcwd())
-    for year in ['2015', '2016', '2017', '2018']:
-        for f in os.listdir(year):
-            if os.path.isdir(year + '/' + f):
-                #print('./' + year + '/' + f)
-                test('./' + year + '/' + f)
+    for d in sorted(os.listdir(os.getcwd())):
+        try:
+            if os.path.isdir(d):
+                int(d)  # check that it is an integer
+                for f in sorted(os.listdir(d)):
+                    if f.startswith('day') and os.path.isdir(d + '/' + f):
+                        # print('./' + d + '/' + f)
+                        test('./' + d + '/' + f)
+        except ValueError:
+            pass  # just ignore
+
 
 if __name__ == '__main__':
     run()
