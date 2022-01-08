f2d79fba1147b1da3651ccfd72b5160ba9b61a51
Patrik Lilja <lilja.patrik@gmail.com>
2022-01-08 17:38:15 +0100

    Add template file

---
+++ template.py
@@ -0,0 +1,24 @@
+import sys
+import re
+import math
+
+from collections import Counter, defaultdict, namedtuple, deque
+from functools import lru_cache
+from itertools import permutations, combinations, chain, cycle, product, islice
+from heapq import heappop, heappush
+
+sys.path.append("../..")
+from common.math_util import *
+from common.timer import *
+from common.util import *
+
+INF = float('inf')
+
+def step1(inp):
+    pass
+
+def read_input():
+    return [line.strip() for line in sys.stdin]
+
+inp = read_input()
+print(step1(inp))
