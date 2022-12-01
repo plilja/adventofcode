c0012c6275aaa95e41476ac32e4a9ae3c28ee022
Patrik Lilja <lilja.patrik@gmail.com>
2022-12-01 16:47:37 +0100

    Day 1 step 2

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -2,16 +2,27 @@ import sys
 
 
 def step1(inp):
-    result = 0
+    return max(parse(inp))
+
+
+def step2(inp):
+    return sum(list(sorted(parse(inp)))[-3:])
+
+
+def parse(inp):
+    result = []
     curr = 0
     for line in inp:
         if line.strip() == '':
+            result.append(curr)
             curr = 0
         else:
             curr += int(line)
-            result = max(result, curr)
+    if curr > 0:
+        result.append(curr)
     return result
 
 
 inp = sys.stdin.readlines()
 print(step1(inp))
+print(step2(inp))
