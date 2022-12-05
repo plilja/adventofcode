c6304b163c8937851aa4959be9909d88a77f7617
Patrik Lilja <lilja.patrik@gmail.com>
2022-12-05 10:11:55 +0100

    Day 3 step 2

--- 2022/day03/day03.py
+++ 2022/day03/day03.py
@@ -2,21 +2,39 @@ import sys
 
 
 def step1(inp):
-    result = 0
+    groups = []
     for line in inp:
         middle = len(line) // 2
-        c1 = {c for c in line[:middle]}
-        c2 = {c for c in line[middle:]}
-        common = c1 & c2
+        groups.append((line[:middle], line[middle:]))
+    return calc(groups)
+
+
+def step2(inp):
+    groups = []
+    for i in range(0, len(inp), 3):
+        groups.append(inp[i:i + 3])
+    return calc(groups)
+
+
+def calc(groups):
+    result = 0
+    for group in groups:
+        common = {c for c in group[0]}
+        for member in group[1:]:
+            c2 = {c for c in member}
+            common = common & c2
         for c in common:
-            if c.islower():
-                prio = ord(c) - ord('a') + 1
-                result += prio
-            else:
-                prio = ord(c) - ord('A') + 27
-                result += prio
+            result += get_prio(c)
     return result
+
+
+def get_prio(c):
+    if c.islower():
+        return ord(c) - ord('a') + 1
+    else:
+        return ord(c) - ord('A') + 27
 
 
 inp = [line.strip() for line in sys.stdin.readlines()]
 print(step1(inp))
+print(step2(inp))
