950c08cd75a6b0e0e8155b1f8a439e93ec45b195
Patrik Lilja <lilja.patrik@gmail.com>
2022-12-06 10:05:27 +0100

    Day 4 step 1

---
+++ 2022/day04/day04.py
@@ -0,0 +1,24 @@
+import sys
+import re
+
+
+def step1(inp):
+    result = 0
+    for (a1, a2), (b1, b2) in inp:
+        if a1 <= b1 <= a2 and a1 <= b2 <= a2:
+            result += 1
+        elif b1 <= a1 <= b2 and b1 <= a2 <= b2:
+            result += 1
+    return result
+
+
+def read_input():
+    result = []
+    for line in sys.stdin:
+        [a1, a2, b1, b2] = re.match(r'(\d+)-(\d+),(\d+)-(\d+)', line).groups()
+        result.append(((int(a1), int(a2)), (int(b1), int(b2))))
+    return result
+
+
+inp = read_input()
+print(step1(inp))
