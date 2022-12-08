cea1d32bb5574d68555543fb5a7810586bce7923
Patrik Lilja <lilja.patrik@gmail.com>
2022-12-08 09:23:44 +0100

    Day 6 step 2

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -2,18 +2,27 @@ from collections import Counter
 
 
 def step1(datastream):
+    return solve(datastream, 4)
+
+
+def step2(datastream):
+    return solve(datastream, 14)
+
+
+def solve(datastream, count):
     counter = Counter()
     for i in range(0, len(datastream)):
         counter[datastream[i]] += 1
-        if i >= 4:
-            c = datastream[i - 4]
+        if i >= count:
+            c = datastream[i - count]
             counter[c] -= 1
             if counter[c] == 0:
                 del counter[c]
-        if i >= 3 and len(counter) == 4:
+        if i >= count - 1 and len(counter) == count:
             return i + 1
     return -1
 
 
 datastream = input()
 print(step1(datastream))
+print(step2(datastream))
