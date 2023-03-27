a02794474e267feed8b154f0fcdd1c0773b64521
Patrik Lilja <lilja.patrik@gmail.com>
2023-03-27 18:45:26 +0200

    Day 11 step 2

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -1,16 +1,32 @@
 import sys
 import re
-from collections import namedtuple, Counter
+from collections import namedtuple, Counter, deque
+from math import gcd
 
 Monkey = namedtuple('Monkey', 'id items operator operator_amount divisor if_true if_false')
 
 
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+
 def step1(monkeys):
+    return simulate(monkeys, 20, 3)
+
+
+def step2(monkeys):
+    return simulate(monkeys, 10000, 1)
+
+
+def simulate(monkeys, rounds, divisor):
+    modder = 1
+    for m in monkeys:
+        modder = lcm(modder, m.divisor)
     monkey_counts = Counter()
-    for i in range(0, 20):
+    for i in range(0, rounds):
         for monkey in monkeys:
             while monkey.items:
-                item = monkey.items.pop(0)
+                item = monkey.items.popleft()
                 monkey_counts[monkey.id] += 1
                 if monkey.operator == '+':
                     if monkey.operator_amount == 'old':
@@ -25,7 +41,8 @@ def step1(monkeys):
                 else:
                     raise ValueError('Unknown operator ' + monkey.operator)
 
-                new_item //= 3
+                new_item //= divisor
+                new_item %= modder
                 if new_item % monkey.divisor == 0:
                     monkeys[monkey.if_true].items.append(new_item)
                 else:
@@ -34,13 +51,12 @@ def step1(monkeys):
     return two_most_common[0][1] * two_most_common[1][1]
 
 
-def read_input():
-    lines = sys.stdin.readlines()
+def parse_input(lines):
     i = 0
     result = []
     while i < len(lines):
         [_, monkey_id] = lines[i].split()
-        items = list(map(int, lines[i + 1].split(': ')[1].split(', ')))
+        items = deque(map(int, lines[i + 1].split(': ')[1].split(', ')))
         [operator, amount] = re.match('\s*Operation: new = old ([+*]) (\d+|old)\s*', lines[i + 2]).groups()
         [divisor] = re.match('\s*Test: divisible by (\d+)\s*', lines[i + 3]).groups()
         [if_true] = re.match('\s*If true: throw to monkey (\d+)\s*', lines[i + 4]).groups()
@@ -50,5 +66,6 @@ def read_input():
     return result
 
 
-monkeys = read_input()
-print(step1(monkeys))
+lines = sys.stdin.readlines()
+print(step1(parse_input(lines)))
+print(step2(parse_input(lines)))
