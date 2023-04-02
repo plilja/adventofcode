97cdcc3620b7abad17cbf0522994c8b509530ebe
Patrik Lilja <lilja.patrik@gmail.com>
2023-04-02 10:28:15 +0200

    Day 13 step 2

--- 2022/day13/day13.py
+++ 2022/day13/day13.py
@@ -1,15 +1,30 @@
 import sys
 import re
+from functools import cmp_to_key
 
 
 def step1(inp):
     result = 0
-    for i in range(0, len(inp)):
-        x, y = inp[i]
+    for i in range(0, len(inp), 2):
+        x = inp[i]
+        y = inp[i + 1]
         t = compare_to(x, y)
         if t <= 0:
-            result += i + 1
+            result += i // 2 + 1
     return result
+
+
+
+def step2(inp):
+    inp_copy = inp
+    divider_packet1 = [[2]]
+    divider_packet2 = [[6]]
+    inp_copy.append(divider_packet1)
+    inp_copy.append(divider_packet2)
+    inp.sort(key=cmp_to_key(compare_to))
+    idx1 = inp_copy.index(divider_packet1) + 1
+    idx2 = inp_copy.index(divider_packet2) + 1
+    return idx1 * idx2
 
 
 def compare_to(x, y):
@@ -34,7 +49,8 @@ def read_input():
     result = []
     lines = sys.stdin.readlines()
     for i in range(0, len(lines), 3):
-        result.append((parse(lines[i].strip()), parse(lines[i + 1].strip())))
+        result.append(parse(lines[i].strip()))
+        result.append(parse(lines[i + 1].strip()))
     return result
 
 
@@ -46,3 +62,4 @@ def parse(line):
 
 inp = read_input()
 print(step1(inp))
+print(step2(inp))
