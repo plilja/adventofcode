865558860ce9d88d4ed16607b8b8657a9fcf6fec
Patrik Lilja <lilja.patrik@gmail.com>
2023-08-19 09:41:32 +0200

    Day 19 step 1

---
+++ 2022/day19/day19.py
@@ -0,0 +1,151 @@
+import sys
+import re
+import heapq
+from collections import namedtuple
+
+Blueprint = namedtuple('Blueprint',
+                       'id ore_robot_ore clay_robot_ore obsidian_robot_ore obsidian_robot_clay geode_robot_ore geode_robot_obsidian')
+State = namedtuple('State', 'ore clay obsidian geode ore_robots clay_robots obsidian_robots geode_robots')
+INF = float('INF')
+
+
+def step1(blueprints):
+    result = 0
+    for blueprint in blueprints:
+        b = evaluate_blueprint(blueprint)
+        result += blueprint.id * b
+    return result
+
+
+def evaluate_blueprint(blueprint):
+    pq = []
+    heapq.heappush(pq, (0, 0, State(0, 0, 0, 0, 1, 0, 0, 0)))
+    cache = {}
+    result = 0
+    while pq:
+        _, minute, state = heapq.heappop(pq)
+        new_ore = state.ore + state.ore_robots
+        new_clay = state.clay + state.clay_robots
+        new_obsidian = state.obsidian + state.obsidian_robots
+        new_geode = state.geode + state.geode_robots
+        prev = cache.get(state, INF)
+        if prev <= minute:
+            continue
+        cache[state] = minute
+        # print(state)
+        if minute == 24:
+            result = max(result, state.geode)
+            continue
+        if approximate_max(state, 24 - minute) <= result:
+            continue
+
+        if state.ore >= blueprint.ore_robot_ore and most_expensive_ore(blueprint) > state.ore_robots:
+            new_state = State(new_ore - blueprint.ore_robot_ore,
+                              new_clay,
+                              new_obsidian,
+                              new_geode,
+                              state.ore_robots + 1,
+                              state.clay_robots,
+                              state.obsidian_robots,
+                              state.geode_robots)
+            heapq.heappush(pq, (rank_state(new_state), minute + 1, new_state))
+        if state.ore >= blueprint.clay_robot_ore and most_expensive_clay(blueprint) > state.clay_robots:
+            new_state = State(new_ore - blueprint.clay_robot_ore,
+                              new_clay,
+                              new_obsidian,
+                              new_geode,
+                              state.ore_robots,
+                              state.clay_robots + 1,
+                              state.obsidian_robots,
+                              state.geode_robots)
+            heapq.heappush(pq, (rank_state(new_state), minute + 1, new_state))
+        if state.ore >= blueprint.obsidian_robot_ore \
+                and state.clay >= blueprint.obsidian_robot_clay \
+                and most_expensive_obsidian(blueprint) > state.obsidian_robots:
+            new_state = State(new_ore - blueprint.obsidian_robot_ore,
+                              new_clay - blueprint.obsidian_robot_clay,
+                              new_obsidian,
+                              new_geode,
+                              state.ore_robots,
+                              state.clay_robots,
+                              state.obsidian_robots + 1,
+                              state.geode_robots)
+            heapq.heappush(pq, (rank_state(new_state), minute + 1, new_state))
+        if state.ore >= blueprint.geode_robot_ore and state.obsidian >= blueprint.geode_robot_obsidian:
+            new_state = State(new_ore - blueprint.geode_robot_ore,
+                              new_clay,
+                              new_obsidian - blueprint.geode_robot_obsidian,
+                              new_geode,
+                              state.ore_robots,
+                              state.clay_robots,
+                              state.obsidian_robots,
+                              state.geode_robots + 1)
+            heapq.heappush(pq, (rank_state(new_state), minute + 1, new_state))
+
+        # new state when we build no new robots and just let existing robots accumulate
+        new_state = State(new_ore,
+                          new_clay,
+                          new_obsidian,
+                          new_geode,
+                          state.ore_robots,
+                          state.clay_robots,
+                          state.obsidian_robots,
+                          state.geode_robots)
+        heapq.heappush(pq, (rank_state(new_state), minute + 1, new_state))
+    return result
+
+
+def approximate_max(state, rem_minutes):
+    """
+    Calculates and upper limit on best possible outcome from this state
+    """
+    upper_limit = state.geode
+    geode_robots = state.geode_robots
+    for i in range(0, rem_minutes):
+        upper_limit += geode_robots
+        geode_robots += 1
+    return upper_limit
+
+
+def rank_state(state):
+    rank = 0
+    rank += 10 * state.geode_robots
+    rank += 5 * state.obsidian_robots
+    rank += state.ore_robots
+    rank += state.clay_robots
+    return -rank
+
+
+def most_expensive_clay(blueprint):
+    return blueprint.obsidian_robot_clay
+
+
+def most_expensive_ore(blueprint):
+    return max(blueprint.ore_robot_ore,
+               blueprint.clay_robot_ore,
+               blueprint.obsidian_robot_ore,
+               blueprint.geode_robot_ore)
+
+
+def most_expensive_obsidian(blueprint):
+    return blueprint.geode_robot_obsidian
+
+
+def read_input():
+    result = []
+    for line in sys.stdin:
+        expr = re.sub('\s+', ' ', '''Blueprint (\d+): 
+                        Each ore robot costs (\d+) ore. 
+                        Each clay robot costs (\d+) ore. 
+                        Each obsidian robot costs (\d+) ore and (\d+) clay. 
+                        Each geode robot costs (\d+) ore and (\d+) obsidian.''')
+        m = re.match(expr, line).groups()
+        [id, ore, clay, obsidian_ore, obsidian_clay, geode_ore, geode_clay] = list(
+            map(int, re.match(expr, line).groups()))
+        result.append(Blueprint(id, ore, clay, obsidian_ore, obsidian_clay, geode_ore, geode_clay))
+    return result
+
+
+blueprints = read_input()
+print('This one is slow. Wait for it...', file=sys.stderr)
+print(step1(blueprints))
