aed99c5cb1caf4d9cff869a44392b0525d521372
Patrik Lilja <lilja.patrik@gmail.com>
2023-08-20 21:50:32 +0200

    Day 20 step 2

--- 2022/day20/day20.py
+++ 2022/day20/day20.py
@@ -1,67 +1,40 @@
 import sys
-from collections import deque
-
-
-def shift(deq, value):
-    deq2 = deque()
-
-    def init_deq2():
-        while deq[0] != value:
-            deq2.append(deq.popleft())
-
-    def shift_left():
-        if deq2:
-            deq.popleft()
-            deq.appendleft(deq2.pop())
-            deq.appendleft(value)
-            if not deq2:
-                shift_left()
-        else:
-            deq.popleft()
-            deq.append(value)
-            init_deq2()
-
-    def shift_right():
-        deq.popleft()
-        if deq:
-            deq2.append(deq.popleft())
-            deq.appendleft(value)
-        else:
-            while deq2:
-                deq.appendleft(deq2.pop())
-            deq.appendleft(value)
-            shift_right()
-
-    v, i = list(map(int, value.split('|')))
-    if v != 0:
-        init_deq2()
-        for _ in range(0, abs(v)):
-            if v < 0:
-                shift_left()
-            else:
-                shift_right()
-
-    while deq2:
-        deq.appendleft(deq2.pop())
 
 
 def step1(inp):
-    ls = deque()
+    return solve(inp, 1, 1)
+
+
+def step2(inp):
+    return solve(inp, 811589153, 10)
+
+
+def solve(inp, decryption_key, iterations):
+    ls = []
     for i, v in enumerate(inp):
         if v == 0:
             zero_key = '{}|{}'.format(0, i)
-        ls.append('{}|{}'.format(v, i))
-    n = len(inp)
-    cp = inp[::]
-    for i, v in enumerate(inp):
-        shift(ls, '{}|{}'.format(v, i))
+        ls.append('{}|{}'.format(v * decryption_key, i))
+    for _ in range(0, iterations):
+        for i, v in enumerate(inp):
+            shift(ls, '{}|{}'.format(v * decryption_key, i))
     zero = ls.index(zero_key)
     result = 0
     for off in [1000, 2000, 3000]:
-        result += int(ls[(zero + off) % n].split('|')[0])
+        result += int(ls[(zero + off) % len(inp)].split('|')[0])
     return result
+
+
+def shift(ls, value):
+    n = len(ls) - 1
+    v, _ = list(map(int, value.split('|')))
+    i = ls.index(value)
+    ls.remove(value)
+    new_i = (i + v) % n
+    ls.insert(new_i, value)
 
 
 if __name__ == '__main__':
     inp = list(map(int, sys.stdin.readlines()))
     print(step1(inp))
+    print(step2(inp))
