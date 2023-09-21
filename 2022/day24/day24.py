de5a1833bd1930001e9ca4144fb8d825eb959b85
Patrik Lilja <lilja.patrik@gmail.com>
2023-09-21 17:14:15 +0200

    Day 24 step 2

--- 2022/day24/day24.py
+++ 2022/day24/day24.py
@@ -1,45 +1,57 @@
 import sys
-
 from heapq import heappop, heappush
 
 sys.path.append("../..")
-from common.timer import *
-from common.util import *
-
-INF = float('inf')
+from common.util import neighbors4
 
 
 def step1(inp):
+    target_x = len(inp[0]) - 2
+    target_y = len(inp) - 1
+    return solve(inp, [(target_x, target_y)])
+
+
+def step2(inp):
+    target_x = len(inp[0]) - 2
+    target_y = len(inp) - 1
+    return solve(inp, [(target_x, target_y), (1, 0), (target_x, target_y)])
+
+
+def solve(inp, waypoints):
     initial_grid = [[[c] if c != '.' else [] for c in s] for s in inp]
-
-    target_y = len(initial_grid) - 1
-    target_x = len(initial_grid[0]) - 2
-
-    q = [(target_y - 0 + target_x - 1, 1, 1, 0)]
+    result = 1
+    start_x = 1
+    start_y = 0
     dist_to_grid = {}
     dist_to_grid[0] = initial_grid
-    visited = set()
+    for target_x, target_y in waypoints:
+        q = [(abs(target_y - start_y) + abs(target_x - start_x), result, start_x, start_y)]
+        visited = set()
 
-    best = float('inf')
-    while q:
-        (to_goal, d, x, y) = heappop(q)
-        if (x, y, d) in visited:
-            continue
-        visited.add((x, y, d))
-        if d + to_goal >= best:
-            continue
-        if d not in dist_to_grid:
-            dist_to_grid[d] = advance_avalanches(dist_to_grid[d - 1])
-        grid = dist_to_grid[d]
-        if grid[y][x]:
-            continue
-        if y == target_y and x == target_x:
-            best = min(best, d)
-            continue
-        heappush(q, (to_goal, d + 1, x, y))  # wait without moving
-        for x2, y2 in neighbors4(x, y):
-            heappush(q, (target_y - y2 + target_x - x2, d + 1, x2, y2))
-    return best
+        best = float('inf')
+        while q:
+            (to_goal, d, x, y) = heappop(q)
+            if (x, y, d) in visited:
+                continue
+            visited.add((x, y, d))
+            if d + to_goal >= best:
+                continue
+            if d not in dist_to_grid:
+                dist_to_grid[d] = advance_avalanches(dist_to_grid[d - 1])
+            grid = dist_to_grid[d]
+            if grid[y][x]:
+                continue
+            if y == target_y and x == target_x:
+                best = min(best, d)
+                continue
+            heappush(q, (to_goal, d + 1, x, y))  # wait without moving
+            for x2, y2 in neighbors4(x, y):
+                if y2 >= 0 and y2 < len(grid):
+                    heappush(q, (abs(target_y - y2) + abs(target_x - x2), d + 1, x2, y2))
+        result = best
+        start_x = target_x
+        start_y = target_y
+    return result
 
 
 def advance_avalanches(grid):
@@ -68,10 +80,7 @@ def advance_avalanches(grid):
     return new_grid
 
 
-def read_input():
-    return [line.strip() for line in sys.stdin]
-
-
 if __name__ == '__main__':
-    grid = read_input()
+    grid = [line.strip() for line in sys.stdin]
     print(step1(grid))
+    print(step2(grid))
